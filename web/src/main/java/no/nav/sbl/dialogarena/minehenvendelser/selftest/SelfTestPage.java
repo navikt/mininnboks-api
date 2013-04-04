2ef809fc50138f453940b93b567a8111ad5f8e34
Børge Bardo <Borge.Bardo@nav.no>
2013-04-04 13:15:30 +0000

    MOD-3558: satt opp selftestside
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@17254 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/selftest/SelfTestPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/selftest/SelfTestPage.java
@@ -1,10 +1,39 @@
 package no.nav.sbl.dialogarena.minehenvendelser.selftest;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.http.WebRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+import java.util.jar.Manifest;
 
 public class SelfTestPage extends WebPage {
 
-    public SelfTestPage() {
+    private static final Logger logger = LoggerFactory.getLogger(SelfTestPage.class);
 
+
+    public SelfTestPage() {
+        String version = getApplicationVersion();
+        add(new Label("version", version));
     }
+
+    private String getApplicationVersion() {
+        String version = "unknown version";
+        try {
+            WebRequest req = (WebRequest) RequestCycle.get().getRequest();
+            ServletContext servletContext = ((HttpServletRequest) req.getContainerRequest()).getServletContext();
+            InputStream inputStream = servletContext.getResourceAsStream(("/META-INF/MANIFEST.MF"));
+            Manifest manifest = new Manifest(inputStream);
+            version = manifest.getMainAttributes().getValue("Implementation-Version");
+        } catch (Exception e) {
+            logger.warn("Feil ved henting av applikasjonsversjon: " + e.getMessage());
+        }
+        return "minehenvendelser - " + version;
+    }
+
 }
