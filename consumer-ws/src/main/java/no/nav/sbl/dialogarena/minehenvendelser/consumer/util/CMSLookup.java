df6fa47c0d8dc49aaa75d0ed1900654ab6f7ed83
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-04-09 08:52:26 +0000

    MOD-3559 La til manglende filer
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@17438 6ec884bf-e345-0410-bd42-d282eb737ba0

---
+++ consumer-ws/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/util/CMSLookup.java
@@ -0,0 +1,8 @@
+package no.nav.sbl.dialogarena.minehenvendelser.consumer.util;
+
+public class CMSLookup {
+
+    public static String lookupText(String textId) {
+        return "CMS-text! " + textId;
+    }
+}
---
+++ consumer-ws/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/util/KodeverkOppslag.java
@@ -0,0 +1,11 @@
+package no.nav.sbl.dialogarena.minehenvendelser.consumer.util;
+
+public final class KodeverkOppslag {
+
+    public static String hentKodeverk(String kodeverkId) {
+        return "KodeverkData " + kodeverkId;
+    }
+
+}
+
+
