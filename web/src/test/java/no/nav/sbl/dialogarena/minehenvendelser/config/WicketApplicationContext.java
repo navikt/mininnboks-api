1c0ba72d952b0becb9d996e2dacbedf954dad1df
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-04-12 08:50:57 +0000

    Checkstyle
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@17657 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/config/WicketApplicationContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/config/WicketApplicationContext.java
@@ -1,12 +1,7 @@
 package no.nav.sbl.dialogarena.minehenvendelser.config;
 
-import no.nav.modig.wicket.test.FluentWicketTester;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import javax.inject.Inject;
-import java.util.Locale;
 
 @Configuration
 @Import({ApplicationContext.class, FluentWicketTesterContext.class})
