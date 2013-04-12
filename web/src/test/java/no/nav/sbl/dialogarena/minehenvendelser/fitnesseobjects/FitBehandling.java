e5ddb98dc15f2e47f02c0aedd78c9649d58a4c2e
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-04-12 13:01:24 +0000

    Mod3558/59 FitNesse - innlesing av datagrunnlag
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@17706 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/fitnesseobjects/FitBehandling.java
+++ web/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/fitnesseobjects/FitBehandling.java
@@ -2,6 +2,8 @@ package no.nav.sbl.dialogarena.minehenvendelser.fitnesseobjects;
 
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.jaxb.Behandling;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.jaxb.Dokumentforventning;
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,16 +38,16 @@ public class FitBehandling {
         return behandling;
     }
 
-    private void populateFieldsFromLists(Behandling behandling) {
+    private void populateSimpleFields(Behandling behandling) {
         setInternalState(behandling, "behandlingsId", behandlingsId);
-        setInternalState(behandling, "dokumentbehandlingType", dokumentbehandlingType);
-        setInternalState(behandling, "brukerbehandlingType", brukerbehandlingType);
-        setInternalState(behandling, "status", status);
-        setInternalState(behandling, "sistEndretDato", sistEndretDato);
-        setInternalState(behandling, "innsendtDato", innsendtDato);
+//        setInternalState(behandling, "behandlingstype", Behandling.BrukerbehandlingType.valueOf(brukerbehandlingType));
+//        setInternalState(behandling, "dokumentbehandlingType", dokumentbehandlingType);
+//        setInternalState(behandling, "status", status);
+        setInternalState(behandling, "sistEndret", new DateTime(ISODateTimeFormat.dateTimeNoMillis().parseDateTime(sistEndretDato)));
+        setInternalState(behandling, "innsendtDato", new DateTime(ISODateTimeFormat.dateTimeNoMillis().parseDateTime(innsendtDato)));
     }
 
-    private void populateSimpleFields(Behandling behandling) {
+    private void populateFieldsFromLists(Behandling behandling) {
         List<Dokumentforventning> dokumentforventninger = new ArrayList<>();
         for (int i = 0; i < kodeverkId.size(); i++) {
             dokumentforventninger.add(createNewDokumentForventningBasedOnLists(i));
@@ -56,10 +58,16 @@ public class FitBehandling {
     private Dokumentforventning createNewDokumentForventningBasedOnLists(int indexOfLists) {
         Dokumentforventning dokumentforventning = new Dokumentforventning();
         setInternalState(dokumentforventning, "kodeverkId", kodeverkId.get(indexOfLists));
-        setInternalState(dokumentforventning, "innsendingsvalg", innsendingsvalg.get(indexOfLists));
-        setInternalState(dokumentforventning, "hovedskjema", hovedskjema.get(indexOfLists));
+//        setInternalState(dokumentforventning, "innsendingsvalg", innsendingsvalg.get(indexOfLists));
+//        setInternalState(dokumentforventning, "hovedskjema", (hovedskjema.get(indexOfLists) == null ? false : hovedskjema.get(indexOfLists)));
         setInternalState(dokumentforventning, "egendefinertTittel", egendefinertTittel.get(indexOfLists));
         return dokumentforventning;
+    }
+
+    public static void main(String ... args) {
+        String xmlDate = "2006-08-19T19:27:14+02:00";
+        DateTime dt = ISODateTimeFormat.dateTimeNoMillis().parseDateTime(xmlDate);
+        System.out.print(dt.toDate());
     }
 
 }
