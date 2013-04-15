e2f3fe9fe046d324b65371ea16650d52a8209034
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-04-15 07:14:45 +0000

    Checkstyle
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@17760 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/util/ParagraphRemover.java
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/util/ParagraphRemover.java
@@ -2,16 +2,15 @@ package no.nav.sbl.dialogarena.minehenvendelser.util;
 
 public class ParagraphRemover {
 
-    public static String remove(String text){
-        if(text.startsWith("<p>")){
+    public static String remove(String input) {
+        String text = input;
+        if (text.startsWith("<p>")) {
             text = text.substring(3);
         }
-        if(text.endsWith("</p>")){
-            text = text.substring(0,text.length()-4);
+        if (text.endsWith("</p>")) {
+            text = text.substring(0, text.length() - 4);
         }
-        
         return text;
-        
     }
-    
+
 }
