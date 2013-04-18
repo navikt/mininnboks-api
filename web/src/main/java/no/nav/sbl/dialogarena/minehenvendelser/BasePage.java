5d439992130bc3f0edfde214c3fe6bfd2e0a2965
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-04-18 08:40:49 +0000

    MOD-3558 Fjernet ubrukt dependency
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@18088 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/BasePage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/BasePage.java
@@ -5,11 +5,7 @@ import no.nav.sbl.dialogarena.common.innstillinger.InnstillingerPanel;
 import no.nav.sbl.dialogarena.common.navigasjon.NavigasjonPanel;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.CmsContentRetriever;
 import no.nav.sbl.dialogarena.minehenvendelser.pages.HomePage;
-import org.apache.wicket.devutils.debugbar.DebugBar;
-import org.apache.wicket.markup.html.TransparentWebMarkupContainer;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
@@ -19,7 +15,6 @@ public class BasePage extends WebPage {
 
     public static final JavaScriptResourceReference JS_RESOURCE = new JavaScriptResourceReference(HomePage.class, "lokal.js");
     public static final CssResourceReference CSS_RESOURCE = new CssResourceReference(HomePage.class, "lokal.css");
-
     @Inject
     protected CmsContentRetriever innholdstekster;
 
