808a2ba096e1942d4575f813488c64499c4bcef3
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-04-24 14:56:01 +0000

    checkstyle
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@18509 6ec884bf-e345-0410-bd42-d282eb737ba0

--- consumer-ws/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/henvendelse/behandling/domain/BrukerBehandlingTransformerTest.java
+++ consumer-ws/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/henvendelse/behandling/domain/BrukerBehandlingTransformerTest.java
@@ -3,7 +3,6 @@ package no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.
 import no.nav.tjeneste.virksomhet.henvendelse.v1.informasjon.WSBrukerBehandling;
 import no.nav.tjeneste.virksomhet.henvendelse.v1.informasjon.WSBrukerBehandlingType;
 import no.nav.tjeneste.virksomhet.henvendelse.v1.informasjon.WSDokumentbehandlingType;
-import org.hamcrest.CoreMatchers;
 import org.joda.time.DateTime;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,14 +12,14 @@ import static org.hamcrest.CoreMatchers.equalTo;
 public class BrukerBehandlingTransformerTest {
 
     @Test
-    public void shouldTransformCorrectly(){
-        WSBrukerBehandling wsBrukerBehandling =  new WSBrukerBehandling()
+    public void shouldTransformCorrectly() {
+        WSBrukerBehandling wsBrukerBehandling = new WSBrukerBehandling()
                 .withBrukerBehandlingType(WSBrukerBehandlingType.DOKUMENT_BEHANDLING)
                 .withBehandlingsId("behandlingId")
                 .withDokumentbehandlingType(WSDokumentbehandlingType.SOKNADSINNSENDING)
                 .withHovedskjemaId("hovedSkjemaId")
-                .withInnsendtDato(new DateTime(2013,01,01,01,01))
-                .withSistEndret(new DateTime(2013,01,02,01,01));
+                .withInnsendtDato(new DateTime(2013, 01, 01, 01, 01))
+                .withSistEndret(new DateTime(2013, 01, 02, 01, 01));
 
         Behandling behandling = Behandling.transformToBehandling(wsBrukerBehandling);
 
