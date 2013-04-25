ad93147668eb70303a7d033504227dd9a5eaeefc
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-04-25 11:07:25 +0000

    Checkstyle
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@18570 6ec884bf-e345-0410-bd42-d282eb737ba0

--- consumer-ws/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/context/ConsumerContext.java
+++ consumer-ws/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/context/ConsumerContext.java
@@ -30,4 +30,6 @@ public class ConsumerContext {
         return proxyFactoryBean.create(HenvendelsesBehandlingPortType.class);
     }
 
+
+
 }
--- consumer-ws/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/ConsumerIntegrationTest.java
+++ consumer-ws/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/ConsumerIntegrationTest.java
@@ -5,23 +5,52 @@ import javax.inject.Inject;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.context.ConsumerTestContext;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.BehandlingService;
 
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.webbitserver.*;
+
+import java.net.URL;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ConsumerTestContext.class})
 public class ConsumerIntegrationTest {
 
+    private WebServer server;
+
     @Inject
     private BehandlingService service;
+
+    @Value("henvendelser.ws.endpoint")
+    private URL endpoint;
 
     @Test
     @Ignore
     public void shouldIntegrateWithHenvendelserViaWebService() {
         service.hentBehandlinger("test");
     }
+
+
+    @Before
+    public void startWebbit(){
+
+        server = WebServers.createWebServer(41001)
+                .add(endpoint.getPath(), new HttpHandler() {
+                    @Override
+                    public void handleHttpRequest(HttpRequest request, HttpResponse response, HttpControl control) throws Exception {
+
+
+                    }
+                });
+
+    }
+
+    @After
+    public void stopWebbit(){
+        server.stop();
+    }
+
 
 }
