e85a908935e59bcea0e2674282b5c8f8b1965219
Stian Landsnes <Stian.Landsnes@nav.no>
2013-04-26 11:22:34 +0000

    MOD-3558, MOD-3559: Konfigurasjon av consumer
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@18683 6ec884bf-e345-0410-bd42-d282eb737ba0

--- wsmock/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/wsmock/config/WsMockApplicationContext.java
+++ wsmock/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/wsmock/config/WsMockApplicationContext.java
@@ -1,16 +1,16 @@
 package no.nav.sbl.dialogarena.minehenvendelser.wsmock.config;
 
-import no.nav.sbl.dialogarena.minehenvendelser.consumer.BehandlingResponseMarshaller;
+import javax.inject.Inject;
+
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.HentBehandlingWebServiceMock;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.MockData;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.context.ConsumerTestContext;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.webbitserver.WebServer;
 import org.webbitserver.WebServers;
-
-import javax.inject.Inject;
 
 @Configuration
 @Import({ConsumerTestContext.class})
@@ -18,8 +18,6 @@ public class WsMockApplicationContext {
 
     @Inject
     private MockData mockData;
-    @Inject
-    private BehandlingResponseMarshaller marshaller;
 
     @Bean
     public WebServer mockSoapServer() {
