ca6ed848f6548beb866b38fe6108835354cd0762
Øyvind Norsveen <Oyvind.Nordsveen@nav.no>
2013-04-26 08:34:07 +0000

    Checkstyle
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@18642 6ec884bf-e345-0410-bd42-d282eb737ba0

--- consumer-ws/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/ConsumerIntegrationTest.java
+++ consumer-ws/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/ConsumerIntegrationTest.java
@@ -3,20 +3,13 @@ package no.nav.sbl.dialogarena.minehenvendelser.consumer;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.context.ConsumerTestContext;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.BehandlingService;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.domain.Behandling;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.webbitserver.WebServer;
-import org.webbitserver.WebServers;
 
 import javax.inject.Inject;
-import java.net.URL;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertNotNull;
@@ -25,8 +18,6 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ConsumerTestContext.class})
 public class ConsumerIntegrationTest {
-
-    private int numTestStarted = 0;
 
     @Inject
     private BehandlingService service;
