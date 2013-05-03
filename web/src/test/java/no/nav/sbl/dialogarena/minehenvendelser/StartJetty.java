b86a22700a329650fef69990e9609e1330ba17f6
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-05-03 13:27:06 +0000

    Kastet ut ubrukt kode
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@19232 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/StartJetty.java
+++ web/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/StartJetty.java
@@ -1,16 +1,12 @@
 package no.nav.sbl.dialogarena.minehenvendelser;
 
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
-import no.nav.sbl.dialogarena.minehenvendelser.consumer.MockData;
-import no.nav.tjeneste.virksomhet.henvendelsesbehandling.v1.HentBrukerBehandlingerResponse;
 
 import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
 import static no.nav.modig.lang.collections.RunnableUtils.first;
 import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
 import static no.nav.modig.test.util.FilesAndDirs.WEBAPP_SOURCE;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
-import static no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.util.MockCreationUtil.createFerdigBehandling;
-import static no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.util.MockCreationUtil.createUnderArbeidBehandling;
 
 public final class StartJetty {
 
@@ -27,14 +23,6 @@ public final class StartJetty {
 
         Jetty jetty = usingWar(WEBAPP_SOURCE).at("minehenvendelser").port(PORT).buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
-    }
-
-    public static MockData createMockData() {
-        MockData mockdata = new MockData();
-        mockdata.addResponse("***REMOVED***", new HentBrukerBehandlingerResponse().withBrukerBehandlinger(createFerdigBehandling(), createUnderArbeidBehandling()));
-
-
-        return mockdata;
     }
 
 }
