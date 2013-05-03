915ae7864a2a86c38c09bcd1c794376eb02a21a2
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-05-03 13:30:55 +0000

    Rydding i kode.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@19234 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/StartJetty.java
+++ web/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/StartJetty.java
@@ -2,24 +2,22 @@ package no.nav.sbl.dialogarena.minehenvendelser;
 
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 
+import static java.lang.System.setProperty;
 import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
 import static no.nav.modig.lang.collections.RunnableUtils.first;
 import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
 import static no.nav.modig.test.util.FilesAndDirs.WEBAPP_SOURCE;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
+import static no.nav.sbl.dialogarena.minehenvendelser.SystemProperties.load;
 
 public final class StartJetty {
 
     public static final int PORT = 8080;
 
-    private StartJetty() {
-    }
-
     public static void main(String[] args) throws Exception {
-
-        System.setProperty("spring.profiles.active", "test");
-        System.setProperty("wicket.configuration", "development");
-        SystemProperties.load("/environment-test.properties");
+        setProperty("spring.profiles.active", "test");
+        setProperty("wicket.configuration", "development");
+        load("/environment-test.properties");
 
         Jetty jetty = usingWar(WEBAPP_SOURCE).at("minehenvendelser").port(PORT).buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
