10c5a669a9708046faebba9739fc8141e0b845e5
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-05-23 09:01:52 +0000

    småfiksing
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@26999 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/StartJetty.java
+++ web/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/StartJetty.java
@@ -1,6 +1,5 @@
 package no.nav.sbl.dialogarena.minehenvendelser;
 
-import no.nav.modig.testcertificates.TestCertificates;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 
 import static java.lang.System.setProperty;
@@ -8,6 +7,7 @@ import static no.nav.modig.lang.collections.FactoryUtils.gotKeypress;
 import static no.nav.modig.lang.collections.RunnableUtils.first;
 import static no.nav.modig.lang.collections.RunnableUtils.waitFor;
 import static no.nav.modig.test.util.FilesAndDirs.WEBAPP_SOURCE;
+import static no.nav.modig.testcertificates.TestCertificates.setupKeyAndTrustStore;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
 import static no.nav.sbl.dialogarena.minehenvendelser.SystemProperties.load;
 
@@ -21,7 +21,7 @@ public final class StartJetty {
         load("/environment-test.properties");
 
         Jetty jetty = usingWar(WEBAPP_SOURCE).at("minehenvendelser").port(PORT).buildJetty();
-        TestCertificates.setupKeyAndTrustStore();
+        setupKeyAndTrustStore();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
     }
 
