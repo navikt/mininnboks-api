e070a7d3ddbd0bce06000b69b7040d36aab16ed4
Børge Bardo <Borge.Bardo@nav.no>
2013-06-24 07:02:54 +0000

    MOD-4020 lagt til aria-label og role paa tooltip-hjelp
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@42883 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/pages/HomePage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/pages/HomePage.java
@@ -1,13 +1,14 @@
 package no.nav.sbl.dialogarena.minehenvendelser.pages;
 
-import no.nav.sbl.dialogarena.minehenvendelser.FoedselsnummerService;
 import no.nav.sbl.dialogarena.minehenvendelser.BasePage;
+import no.nav.sbl.dialogarena.minehenvendelser.FoedselsnummerService;
 import no.nav.sbl.dialogarena.minehenvendelser.components.BehandlingerUnderArbeidListView;
 import no.nav.sbl.dialogarena.minehenvendelser.components.FerdigeBehandlingerListView;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.BehandlingService;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.domain.Behandling;
 import no.nav.sbl.dialogarena.webkomponent.tilbakemelding.service.TilbakemeldingService;
 import no.nav.sbl.dialogarena.webkomponent.tilbakemelding.web.TilbakemeldingContainer;
+import org.apache.wicket.Component;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
@@ -19,7 +20,6 @@ import java.util.List;
 import static java.lang.System.getProperty;
 import static no.nav.modig.wicket.conditional.ConditionalUtils.visibleIf;
 import static no.nav.modig.wicket.model.ModelUtils.isEmpty;
-import static org.apache.wicket.model.Model.of;
 
 /**
  * Hovedside for applikasjonen. Laster inn behandlinger via. en service og
@@ -44,7 +44,7 @@ public class HomePage extends BasePage {
 
         add(
                 new Label("hovedTittel", innholdstekster.hentTekst("hoved.tittel")),
-                new WebMarkupContainer("tooltip").add(new AttributeAppender("title", of(innholdstekster.hentTekst("tooltip.tekst")))),
+                createTooltip(),
                 new Label("skjultOverskriftPaabegynte", innholdstekster.hentTekst("skjult.overskrift.paabegynte")),
                 new BehandlingerUnderArbeidListView("behandlingerUnderArbeid", behandlinger),
                 new Label("skjultOverskriftKvitteringer", innholdstekster.hentTekst("skjult.overskrift.kvitteringer")),
@@ -53,6 +53,12 @@ public class HomePage extends BasePage {
                 new ExternalLink("forsiden", getProperty("inngangsporten.link.url"), innholdstekster.hentTekst("link.tekst.forsiden")),
                 new TilbakemeldingContainer("panel-tilbakemelding", tilbakemeldingService, tilbakemeldingEnabled)
         );
+    }
+
+    private Component createTooltip() {
+        return new WebMarkupContainer("tooltip")
+                .add(new AttributeAppender("title", innholdstekster.hentTekst("tooltip.hjelpetekst")))
+                .add(new AttributeAppender("aria-label", innholdstekster.hentTekst("skjult.label.hjelpetekst")));
     }
 
     private class IngenBehandlingerView extends WebMarkupContainer {
