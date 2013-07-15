d521837c290dbef2e51b19648be937518b10e822
Frithjof Nerdrum <Frithjof.Nerdrum@nav.no>
2013-07-15 12:55:42 +0000

    Spørsmål og svarlista blir nå oppdatert hver gang bruker stiller et nytt spørsmål.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@44347 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/pages/SporsmalSide.java
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/pages/SporsmalSide.java
@@ -1,5 +1,9 @@
 package no.nav.sbl.dialogarena.minehenvendelser.pages;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
 import no.nav.sbl.dialogarena.minehenvendelser.BasePage;
 import no.nav.tjeneste.domene.brukerdialog.sporsmalogsvar.v1.SporsmalOgSvarPortType;
 import no.nav.tjeneste.domene.brukerdialog.sporsmalogsvar.v1.informasjon.WSSporsmal;
@@ -12,12 +16,7 @@ import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.util.ListModel;
-
-import javax.inject.Inject;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.wicket.model.LoadableDetachableModel;
 
 import static java.util.Arrays.asList;
 
@@ -35,23 +34,22 @@ public class SporsmalSide extends BasePage {
 
     public SporsmalSide() {
         add(new SporsmalForm("sporsmalForm", new CompoundPropertyModel<>(new Sporsmal())));
-        final List<WSSporsmalOgSvar> sporsmalOgSvar = sporsmalOgSvarService.hentSporsmalOgSvarListe("***REMOVED***");
 
-        ListModel<Sporsmal> sporsmalListeModell = new ListModel<>(sporsmal);
-        for (WSSporsmalOgSvar ss : sporsmalOgSvar) {
-            Sporsmal s = new Sporsmal();
-            s.sporsmalString = ss.getSporsmal();
-            s.svar = ss.getSvar();
-            sporsmal.add(s);
-        }
-        PropertyListView<Sporsmal> liste = new PropertyListView<Sporsmal>("sporsmalliste", sporsmalListeModell) {
+        PropertyListView<WSSporsmalOgSvar> liste = new PropertyListView<WSSporsmalOgSvar>("sporsmalliste", new AlleSporsmalOgSvar()) {
             @Override
-            protected void populateItem(ListItem<Sporsmal> item) {
-                item.add(new Label("sporsmalString", item.getModelObject().sporsmalString));
-                item.add(new Label("svar", item.getModelObject().svar));
+            protected void populateItem(ListItem<WSSporsmalOgSvar> item) {
+                item.add(new Label("sporsmalString", item.getModelObject().getSporsmal()));
+                item.add(new Label("svar", item.getModelObject().getSvar()));
             }
         };
         add(liste);
+    }
+
+    private class AlleSporsmalOgSvar extends LoadableDetachableModel<List<WSSporsmalOgSvar>> {
+        @Override
+        protected List<WSSporsmalOgSvar> load() {
+            return sporsmalOgSvarService.hentSporsmalOgSvarListe("***REMOVED***");
+        }
     }
 
     private final class SporsmalForm extends Form<Sporsmal> {
