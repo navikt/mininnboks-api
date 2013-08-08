30b747e7ccc9c3869526b7e21f6fc8fa8cdc51bc
Thomas Fossum <Thomas.Fossum@nav.no>
2013-08-08 08:15:02 +0000

    Unused imports
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@71077 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/components/sporsmalogsvar/SendSporsmalPanel.java
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/components/sporsmalogsvar/SendSporsmalPanel.java
@@ -10,8 +10,6 @@ import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.event.Broadcast;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.panel.Panel;
