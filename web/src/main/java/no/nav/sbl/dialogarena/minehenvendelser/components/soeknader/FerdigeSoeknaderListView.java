3eb4236f0259478a6ce16ee421fe06a913cc0603
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-08-09 10:02:39 +0000

    La til glemt klasse
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@71197 6ec884bf-e345-0410-bd42-d282eb737ba0

---
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/components/soeknader/FerdigeSoeknaderListView.java
@@ -0,0 +1,23 @@
+package no.nav.sbl.dialogarena.minehenvendelser.components.soeknader;
+
+import no.nav.sbl.dialogarena.minehenvendelser.consumer.sakogbehandling.domain.Soeknad;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+
+import java.util.List;
+
+public class FerdigeSoeknaderListView extends SoeknaderListView {
+
+    public FerdigeSoeknaderListView(String id, List<? extends Soeknad> soeknader) {
+        super(id, soeknader);
+    }
+
+    @Override
+    protected void populateItem(ListItem<Soeknad> item) {
+        item.add(
+                new Label("tema", item.getModelObject().getTema()),
+                new Label("beskrivelse", item.getModelObject().getBeskrivelse())
+        );
+    }
+
+}
