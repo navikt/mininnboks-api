2cae6ebfb24c6b591fa58206d5cddf363c7474e9
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-08-14 10:12:03 +0000

    Slettet unødvendig kode
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@71493 6ec884bf-e345-0410-bd42-d282eb737ba0

--- consumer/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/integration/HenvendelseConsumerIntegrationTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/integration/HenvendelseConsumerIntegrationTest.java
@@ -4,9 +4,8 @@ import no.nav.sbl.dialogarena.minehenvendelser.consumer.MockData;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.context.HenvendelseConsumerTestContext;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.BehandlingService;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.domain.Behandling;
-import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.informasjon.WSBrukerBehandlingOppsummering;
+import no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.util.MockCreationUtil;
 import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.meldinger.HentBrukerBehandlingListeResponse;
-import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,14 +14,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.inject.Inject;
 import java.util.List;
 
-import static no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.util.MockCreationUtil.KODEVERK_ID_1;
-import static no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.util.MockCreationUtil.KODEVERK_ID_2;
-import static no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.util.MockCreationUtil.KODEVERK_ID_3;
-import static no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.util.MockCreationUtil.createWSDokumentForventningMock;
-import static no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.util.MockCreationUtil.createWsBehandlingMock;
-import static no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.informasjon.WSBehandlingsstatus.FERDIG;
-import static no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.informasjon.WSInnsendingsValg.LASTET_OPP;
-import static no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.informasjon.WSInnsendingsValg.SENDES_IKKE;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
@@ -39,7 +30,7 @@ public class HenvendelseConsumerIntegrationTest {
 
     @Test
     public void shouldIntegrateWithHenvendelserViaWebService() {
-        mockdata.getHentData().addResponse("***REMOVED***", new HentBrukerBehandlingListeResponse().withBrukerBehandlinger(createFerdigBehandling()));
+        mockdata.getHentData().addResponse("***REMOVED***", new HentBrukerBehandlingListeResponse().withBrukerBehandlinger(MockCreationUtil.createUnderArbeidBehandling()));
         List<Behandling> behandlingList = service.hentBehandlinger("***REMOVED***");
         assertNotNull(behandlingList);
         assertThat(behandlingList.size(), equalTo(1));
@@ -47,18 +38,10 @@ public class HenvendelseConsumerIntegrationTest {
 
     @Test
     public void ukjentBrukerSkalGiTomListe() {
-        mockdata.getHentData().addResponse("tester", new HentBrukerBehandlingListeResponse().withBrukerBehandlinger(createFerdigBehandling()));
+        mockdata.getHentData().addResponse("tester", new HentBrukerBehandlingListeResponse().withBrukerBehandlinger(MockCreationUtil.createUnderArbeidBehandling()));
         List<Behandling> behandlingList = service.hentBehandlinger("test1");
         assertNotNull(behandlingList);
         assertThat(behandlingList.size(), equalTo(0));
     }
 
-    private static WSBrukerBehandlingOppsummering createFerdigBehandling() {
-        WSBrukerBehandlingOppsummering wsBehandlingMock = createWsBehandlingMock(new DateTime(2013, 1, 2, 1, 1), new DateTime(2013, 1, 2, 1, 1), FERDIG, false);
-        wsBehandlingMock.getDokumentForventningOppsummeringer().withDokumentForventningOppsummering(
-                createWSDokumentForventningMock(true, KODEVERK_ID_1, LASTET_OPP),
-                createWSDokumentForventningMock(false, KODEVERK_ID_2, LASTET_OPP),
-                createWSDokumentForventningMock(false, KODEVERK_ID_3, SENDES_IKKE));
-        return wsBehandlingMock;
-    }
 }
