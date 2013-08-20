b85a6b35dea12f81e827b8d3c19f9c1cc7bc7abe
Jarle Svendsrud <Jarle.Svendsrud@nav.no>
2013-08-20 08:08:01 +0000

    Fikset integrasjonstest
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@71965 6ec884bf-e345-0410-bd42-d282eb737ba0

--- consumer/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/integration/SakOgbehandlingIntegrationTest.java
+++ consumer/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/integration/SakOgbehandlingIntegrationTest.java
@@ -4,8 +4,10 @@ import no.nav.sbl.dialogarena.minehenvendelser.consumer.MockData;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.context.SakogbehandlingIntegrationTestContext;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.sakogbehandling.SakogbehandlingService;
 import no.nav.sbl.dialogarena.minehenvendelser.consumer.sakogbehandling.domain.Soeknad;
-import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.informasjon.WSBrukerBehandling;
+import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.informasjon.WSBrukerBehandlingOppsummering;
+import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.informasjon.WSDokumentForventningOppsummeringer;
 import no.nav.tjeneste.domene.brukerdialog.henvendelsesbehandling.v1.HenvendelsesBehandlingPortType;
+import no.nav.tjeneste.virksomhet.sakogbehandling.v1.HentBehandlingskjedensBehandlingerResponse;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Applikasjoner;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Avslutningsstatuser;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Behandling;
@@ -15,15 +17,15 @@ import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Behandlingssteg
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Behandlingstid;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.Behandlingstyper;
 import no.nav.tjeneste.virksomhet.sakogbehandling.v1.informasjon.hentbehandlingskjedensbehandlinger.Behandlingskjede;
-import no.nav.tjeneste.virksomhet.sakogbehandling.v1.HentBehandlingskjedensBehandlingerResponse;
+import org.joda.time.DateTime;
 import org.junit.After;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.Assert.assertNotNull;
@@ -34,6 +36,7 @@ import static no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behan
 import static no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.util.MockCreationUtil.populateFinnbehandlingKjedeListWithThreeFerdige;
 import static no.nav.sbl.dialogarena.minehenvendelser.consumer.henvendelse.behandling.util.MockCreationUtil.populateFinnbehandlingKjedeListWithTwoUnderArbeid;
 import static no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.informasjon.WSBehandlingsstatus.FERDIG;
+import static no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.informasjon.WSBrukerBehandlingType.DOKUMENT_BEHANDLING;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
@@ -72,7 +75,6 @@ public class SakOgbehandlingIntegrationTest {
         assertThat(soeknadList.size(), equalTo(2));
     }
 
-    @Ignore
     @Test
     public void verifyNumberOfMottatteSoeknader() {
         setupMottattSoeknad(AKTOR_ID);
@@ -103,16 +105,28 @@ public class SakOgbehandlingIntegrationTest {
                 .withAvslutningsstatus(new Avslutningsstatuser().withValue("avslutningsstatus"))
                 .withNormertBehandlingstid(new Behandlingstid())
                 .withFrist(createXmlGregorianDate(1, 3, 2013));
+
         return new Behandling[]{behandling};
     }
 
     private String setupHenvendelseForMottattSoeknad(String aktorId) {
         String behandlingsId= "BEHID1";
-        when(henvendelsesBehandlingPortType.hentBrukerBehandling(aktorId)).thenReturn(
-                new WSBrukerBehandling()
-                        .withBehandlingsId(behandlingsId)
-                        .withStatus(FERDIG));
+        when(henvendelsesBehandlingPortType.hentBrukerBehandlingListe(aktorId)).thenReturn(createBrukerbehandling(behandlingsId));
         return behandlingsId;
+    }
+
+    private List<WSBrukerBehandlingOppsummering> createBrukerbehandling(String behandlingsId) {
+        List<WSBrukerBehandlingOppsummering> brukerBehandlingOppsummeringer = new ArrayList<>();
+        brukerBehandlingOppsummeringer.add(
+                new WSBrukerBehandlingOppsummering()
+                        .withStatus(FERDIG)
+                        .withBehandlingsId(behandlingsId)
+                        .withHovedskjemaId("id")
+                        .withBrukerBehandlingType(DOKUMENT_BEHANDLING)
+                        .withSistEndret(new DateTime())
+                        .withDokumentForventningOppsummeringer(new WSDokumentForventningOppsummeringer())
+        );
+        return brukerBehandlingOppsummeringer;
     }
 
 }
