b9ec129250cbcb872815432ac10dacb4bdd2bc24
Hans Olaf Borch <Hans.Olav.Borch@nav.no>
2013-08-22 08:57:59 +0000

    [SWITCH] fjernet gammel "minehenvendelser"
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@72220 6ec884bf-e345-0410-bd42-d282eb737ba0

--- consumer/src/test/java/no/nav/sbl/dialogarena/minehenvendelser/consumer/sakogbehandling/soap/Header.java
+++
@@ -1,7 +0,0 @@
-package no.nav.sbl.dialogarena.minehenvendelser.consumer.sakogbehandling.soap;
-
-import javax.xml.bind.annotation.XmlType;
-
-@XmlType(name = "Header", namespace = Envelope.HTTP_SCHEMAS_XMLSOAP_ORG_SOAP_ENVELOPE)
-public class Header {
-}
