4ca23e5ee6587828e731f1380563d6d3b577c1f4
Henrik Hellerøy <Henrik.Helleroy@nav.no>
2013-09-09 15:29:14 +0000

    MOD-1669: La til funksjonalitet for å forminske og utvide tidligere henvendelser.
    git-svn-id: https://versjonskontroll.adeo.no/svn/nav-sbl/dialogarena/minehenvendelser/trunk@73590 6ec884bf-e345-0410-bd42-d282eb737ba0

--- web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/henvendelser/innboks/innboks.js
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/henvendelser/innboks/innboks.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
     var attachTilInnboksListener = function() {
         $('.tilbake-til-innboks-link').on('click', function() {
-          attachAjaxCompleteListener();
+            attachAjaxCompleteListener();
         });
     };
 
@@ -20,16 +20,40 @@ $(document).ready(function() {
             $('#meldinger').scrollTop(distanseFraToppen);
             attachMeldingListener();
             attachTilInnboksListener();
+            attachToggleHoydeListener();
+        });
+    };
+
+    var attachToggleHoydeListener = function() {
+        var $tidligereHenvendelse = $('.tidligere-henvendelse p');
+        var $utvideTekstPil = $('.utvide-tekst-pil');
+        var minHoyde = 50;
+
+        $tidligereHenvendelse.each(function() {
+            $(this).data('height', $(this).height());
+        });
+
+        $tidligereHenvendelse.height(minHoyde);
+
+        $utvideTekstPil.on('click', function() {
+            var $tekstFelt = $(this).siblings('p');
+            if($tekstFelt.height() == minHoyde) {
+                $tekstFelt.animate({height: $tekstFelt.data('height')});
+            } else {
+                $tekstFelt.animate({height: minHoyde});
+            }
+            $(this).toggleClass('rotert');
         });
     };
 
     var adjustInnboksHeight = function() {
         var bodyHeight = $('body').outerHeight();
         var restHeight = $('.footer').outerHeight() + $('.innstillinger-innlogget').outerHeight() +
-                         $('.rad-logo').outerHeight() + $('#innboks-top').outerHeight();
+            $('.rad-logo').outerHeight() + $('#innboks-top').outerHeight();
         $('#innboks-container').height(bodyHeight - restHeight);
     };
 
+    attachToggleHoydeListener();
     attachMeldingListener();
     attachTilInnboksListener();
     adjustInnboksHeight();
