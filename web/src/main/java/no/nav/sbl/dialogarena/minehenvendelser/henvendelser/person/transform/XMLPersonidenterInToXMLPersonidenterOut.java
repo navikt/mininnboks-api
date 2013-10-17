645cd97bbe2c18e0728f48e9250829c19038476c
Anders Hefre <anders.hefre@nav.no>
2013-10-17 17:37:21 +0200

    [TPS] Forenkler prosessen med å vise og oppdatere.

---
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/henvendelser/person/transform/XMLPersonidenterInToXMLPersonidenterOut.java
@@ -0,0 +1,11 @@
+package no.nav.sbl.dialogarena.minehenvendelser.henvendelser.person.transform;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: h138603
+ * Date: 17.10.13
+ * Time: 15:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class XMLPersonidenterInToXMLPersonidenterOut {
+}
