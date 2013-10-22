a95823d428c0ae6e796899d6304dfb4500931e18
Anders Hefre <anders.hefre@nav.no>
2013-10-22 09:25:19 +0200

    [TPS] Mer opprydding og fjerning av kode. Det som gjenstår er å populere adresser og elektroniske kanaler, men mulig man burde vente med dette til å høre hvordan det blir med TPS og oppdater-operasjon fra deres WS.

--- web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/henvendelser/person/transform/Transform.java
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/henvendelser/person/transform/Transform.java
@@ -1,29 +1,9 @@
 package no.nav.sbl.dialogarena.minehenvendelser.henvendelser.person.transform;
 
-import no.nav.modig.lang.option.Optional;
 import no.nav.tjeneste.virksomhet.behandlebrukerprofil.v1.feil.XMLForretningsmessigUnntak;
 import org.apache.commons.collections15.Transformer;
 
 public final class Transform {
-
-    /**
-     * Transformer som caster til en angitt type dersom dette er mulig. Hvis ikke
-     * så returneres null. Dette gjør det mulig å "short-circuit'e" en map-operasjon
-     * med {@link Optional}.
-     *
-     * @param <T> en type man vil caste til
-     * @param type typen man vil caste til.
-     * @return Samme instans castet til subtype, eller <code>null</code> dersom casting ikke er mulig.
-     *
-     */
-    public static <T> Transformer<Object, T> castIfPossibleTo(final Class<T> type) {
-        return new Transformer<Object, T>() {
-            @Override
-            public T transform(Object value) {
-                return type.isInstance(value) ? type.cast(value) : null;
-            }
-        };
-    }
 
     public static Transformer<XMLForretningsmessigUnntak, String> feilaarsakkode() {
         return new XMLUnntakTransform.Aarsakkode();
