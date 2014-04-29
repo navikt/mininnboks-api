ab24fd4cdf85844adb98eaab2585019c32428fb2
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2014-04-29 13:51:34 +0200

    Implementert KvitteringPage

--- web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/sporsmal/kvittering/KvitteringPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/minehenvendelser/sporsmal/kvittering/KvitteringPage.java
@@ -1,10 +1,14 @@
 package no.nav.sbl.dialogarena.minehenvendelser.sporsmal.kvittering;
 
 import no.nav.sbl.dialogarena.minehenvendelser.BasePage;
+import no.nav.sbl.dialogarena.minehenvendelser.innboks.Innboks;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.ExternalLink;
 
 public class KvitteringPage extends BasePage {
 
     public KvitteringPage() {
-        add(new KvitteringPanel("kvittering"));
+        add(new ExternalLink("til-inngangsporten", System.getProperty("inngangsporten.link.url")));
+        add(new BookmarkablePageLink<Void>("til-mine-henvendelser", Innboks.class));
     }
 }
