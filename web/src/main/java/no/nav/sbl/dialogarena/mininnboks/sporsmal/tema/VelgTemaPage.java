e03ca1af5c1ae8c700b781d9e9ceab9d7048dd35
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-06-10 16:07:29 +0200

    Gjort VelgTemaPage mer testbar. Økt testdekning av VelgTemaPage.

--- web/src/main/java/no/nav/sbl/dialogarena/mininnboks/sporsmal/tema/VelgTemaPage.java
+++ web/src/main/java/no/nav/sbl/dialogarena/mininnboks/sporsmal/tema/VelgTemaPage.java
@@ -4,80 +4,60 @@ import no.nav.sbl.dialogarena.mininnboks.BasePage;
 import no.nav.sbl.dialogarena.mininnboks.innboks.Innboks;
 import no.nav.sbl.dialogarena.mininnboks.sporsmal.send.SkrivPage;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.form.AjaxFormChoiceComponentUpdatingBehavior;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.Radio;
 import org.apache.wicket.markup.html.form.RadioGroup;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
-import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import static java.util.Arrays.asList;
 
 public class VelgTemaPage extends BasePage {
 
-    private final FeedbackPanel feedback;
-    private Tema tema = null;
+    private IModel<Tema> tema = new Model<>();
 
     public VelgTemaPage() {
-        feedback = new FeedbackPanel("feedback");
+        final FeedbackPanel feedback = new FeedbackPanel("feedback");
         feedback.setOutputMarkupId(true);
-        add(feedback);
-        add(new TemaValg("temavalg"));
-        add(new Fortsettknapp("fortsett"));
+
+        Form form = new Form<>("temavalg");
+        form.add(new RadioGroup<>("tema", tema)
+                .setRequired(true)
+                .add(new ListView<Tema>("temaer", asList(Tema.values())) {
+                    @Override
+                    protected void populateItem(ListItem<Tema> item) {
+                        item.add(
+                                new Radio<>("valg", item.getModel()),
+                                new Label("navn", new ResourceModel(item.getModelObject().toString())),
+                                new Label("beskrivelse", new ResourceModel(item.getModelObject() + ".beskrivelse")));
+                    }
+                }));
+        form.add(new AjaxSubmitLink("fortsett") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                setResponsePage(SkrivPage.class, new PageParameters().set("tema", tema.getObject().name()));
+            }
+            @Override
+            protected void onError(AjaxRequestTarget target, Form<?> form) {
+                target.add(feedback);
+            }
+        });
+        form.add(feedback);
+
+        add(form);
         add(new Link<Void>("avbryt") {
             @Override
             public void onClick() {
                 setResponsePage(Innboks.class);
             }
         });
-    }
-
-
-    private final class Fortsettknapp extends AjaxLink<Void> {
-
-        public Fortsettknapp(String id) {
-            super(id);
-        }
-
-        @Override
-        public void onClick(AjaxRequestTarget target) {
-            if (tema != null) {
-                setResponsePage(SkrivPage.class, new PageParameters().set("tema", tema.name()));
-            } else {
-                error(new StringResourceModel("send-sporsmal.temavelger.ikke-valgt-tema", VelgTemaPage.this, null).getString());
-                target.add(feedback);
-            }
-        }
-
-    }
-
-    private final class TemaValg extends RadioGroup<Tema> {
-
-        public TemaValg(String id) {
-            super(id, new Model<Tema>());
-            add(new ListView<Tema>("temaer", asList(Tema.values())) {
-                @Override
-                protected void populateItem(ListItem<Tema> item) {
-                    item.add(
-                            new Radio<>("valg", item.getModel()),
-                            new Label("navn", new ResourceModel(item.getModelObject().toString())),
-                            new Label("beskrivelse", new ResourceModel(item.getModelObject() + ".beskrivelse")));
-                }
-            });
-            add(new AjaxFormChoiceComponentUpdatingBehavior() {
-                @Override
-                protected void onUpdate(AjaxRequestTarget target) {
-                    tema = TemaValg.this.getModelObject();
-                }
-            });
-        }
-
     }
 }
--- web/src/test/java/no/nav/sbl/dialogarena/mininnboks/sporsmal/tema/VelgTemaPageTest.java
+++ web/src/test/java/no/nav/sbl/dialogarena/mininnboks/sporsmal/tema/VelgTemaPageTest.java
@@ -2,8 +2,9 @@ package no.nav.sbl.dialogarena.mininnboks.sporsmal.tema;
 
 import no.nav.sbl.dialogarena.mininnboks.WicketPageTest;
 import no.nav.sbl.dialogarena.mininnboks.innboks.Innboks;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.markup.html.form.RadioGroup;
+import no.nav.sbl.dialogarena.mininnboks.sporsmal.send.SkrivPage;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.link.Link;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,9 +21,20 @@ public class VelgTemaPageTest extends WicketPageTest {
 
     @Test
     public void testVelgTemaKomponenter() {
-        wicketTester.should().containComponent(ofType(RadioGroup.class).and(withId("temavalg")))
-                .should().containComponent(ofType(AjaxLink.class).and(withId("fortsett")))
+        wicketTester.should().containComponent(ofType(Form.class).and(withId("temavalg")))
+                .should().inComponent(withId("temavalg")).containComponent(ofType(AjaxSubmitLink.class).and(withId("fortsett")))
                 .should().containComponent(ofType(Link.class).and(withId("avbryt")));
+    }
+
+    @Test
+    public void testFortsettMedValgtTema() {
+        wicketTester.inForm(withId("temavalg")).select("tema", 0).andReturn()
+                .click().link(withId("fortsett")).should().beOn(SkrivPage.class);
+    }
+
+    @Test
+    public void testFortsettUtenValgtTema() {
+        wicketTester.click().link(withId("fortsett")).should().beOn(VelgTemaPage.class);
     }
 
     @Test
