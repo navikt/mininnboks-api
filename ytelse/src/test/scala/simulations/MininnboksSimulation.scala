6c9a30af528f0e7bbc52459305b73a8720a336cd
Peter Hemmen <peter.hemmen@nav.no>
2014-07-08 10:37:10 +0200

    Fjerner pausene og sjekker ordentlig for at spørsmålet gikk OK å sende inn.

--- ytelse/src/test/scala/simulations/MininnboksSimulation.scala
+++ ytelse/src/test/scala/simulations/MininnboksSimulation.scala
@@ -3,8 +3,6 @@ package simulations
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-import scala.concurrent.duration._
-
 class MininnboksSimulation extends Simulation {
 
   final val ENV = "t11"
@@ -39,43 +37,37 @@ class MininnboksSimulation extends Simulation {
     .get("https://tjenester-" + ENV + ".nav.no/esso/UI/Login?goto=https://tjenester-" + ENV + ".nav.no/mininnboks/&service=level4Service")
     .headers(standard_headers)
     .check(regex("OpenAM").exists))
-    .pause(125 milliseconds)
 
     .exec(http("logger inn")
     .post("/esso/UI/Login")
     .headers(standard_headers)
     .param("IDToken1", "***REMOVED***")
     .param("IDToken2", "Eifel123")
-    .queryParam("goto", "https://tjenester-" + ENV + ".nav.no/mininnboks/")
+    .queryParam("goto", goTo)
     .check(regex("Min Innboks").exists))
-    .pause(999 milliseconds)
 
     .exec(http("test om jeg er logget inn")
     .get( """/mininnboks/""")
     .headers(standard_headers)
     .check(regex("Min Innboks").exists))
-    .pause(999 milliseconds)
 
     .exec(http("Tar en snarvei til siden der man skriver inn spørsmål")
     .get( """/mininnboks/sporsmal/skriv/HJELPEMIDLER""")
     .headers(standard_headers)
     .check(regex("Skriv melding").exists))
-    .pause(350 milliseconds)
 
     .exec(http("Sender det faktiske spørsmålet")
     .post( """/mininnboks/sporsmal/skriv/HJELPEMIDLER?2-1.IBehaviorListener.0-sporsmal~form-send=""")
     .headers(headers_8)
     .param( """id40_hf_0""", """""")
-    .param( """tekstfelt:text""", """Dette er en melding som er sendt av gatling. Er den ikke fin?
-
-Tjohei og tjobing!""")
+    .param( """tekstfelt:text""", """Dette er en melding som er sendt av gatling. Er den ikke fin?""")
     .param( """send""", """1""")
-    .check(regex("Takk, du vil få svar").exists))
-    .pause(193 milliseconds)
+    .check(regex("kvittering").exists))
 
     .exec(http("Logger ut")
     .get( """/esso/UI/Logout""")
-    .headers(standard_headers))
+    .headers(standard_headers)
+    .check(regex("You are logged out").exists))
 
 
   setUp(scn.inject(atOnce(1 user))).protocols(httpProtocol)
