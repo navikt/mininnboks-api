56430e683038b3c98d9754e375678652bcde124c
Eirik L. Wang <eirik.wang@nav.no>
2014-09-01 16:04:23 +0200

    TEMP: Mocket diskresjonskode for å teste. Legg tilbake når koden er på beina

--- web/src/main/java/no/nav/sbl/dialogarena/mininnboks/config/ApplicationContext.java
+++ web/src/main/java/no/nav/sbl/dialogarena/mininnboks/config/ApplicationContext.java
@@ -19,6 +19,8 @@ import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.innsynhenvendelse.Inns
 import no.nav.tjeneste.domene.brukerdialog.henvendelse.v1.sendinnhenvendelse.SendInnHenvendelsePortType;
 import no.nav.tjeneste.domene.brukerdialog.henvendelse.v2.henvendelse.HenvendelsePortType;
 import no.nav.tjeneste.pip.diskresjonskode.DiskresjonskodePortType;
+import no.nav.tjeneste.pip.diskresjonskode.meldinger.HentDiskresjonskodeRequest;
+import no.nav.tjeneste.pip.diskresjonskode.meldinger.HentDiskresjonskodeResponse;
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.feature.LoggingFeature;
@@ -118,12 +120,23 @@ public class ApplicationContext implements ApplicationContextAware {
                 false);
     }
 
-    @Bean
+    /**@Bean
     public DiskresjonskodePortType diskresjonskodePortType() {
         return createPortType(System.getProperty("diskresjonskode.ws.url"),
                 "classpath:wsdl/Diskresjonskode.wsdl",
                 DiskresjonskodePortType.class,
                 true);
+    }**/
+    @Bean
+    public DiskresjonskodePortType diskresjonskodePortType() {
+        return new DiskresjonskodePortType() {
+            @Override
+            public HentDiskresjonskodeResponse hentDiskresjonskode(HentDiskresjonskodeRequest request) {
+                HentDiskresjonskodeResponse hentDiskresjonskodeResponse = new HentDiskresjonskodeResponse();
+                hentDiskresjonskodeResponse.setDiskresjonskode("7");
+                return hentDiskresjonskodeResponse;
+            }
+        };
     }
 
     @Bean
