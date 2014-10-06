a0c7523ecf9ff2ab132282f6cc0480e3f140e3a5
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-10-06 09:17:18 +0200

    Ryddet opp i test-context da det ikke fungerte å kontakte Henvendelse lokalt.

--- web/src/test/java/no/nav/sbl/dialogarena/mininnboks/config/EpostServiceMockContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/mininnboks/config/EpostServiceMockContext.java
@@ -1,6 +1,6 @@
-package no.nav.sbl.dialogarena.mininnboks.consumer;
+package no.nav.sbl.dialogarena.mininnboks.config;
 
-import no.nav.sbl.dialogarena.mininnboks.config.MockApplicationContext;
+import no.nav.sbl.dialogarena.mininnboks.consumer.EpostService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- web/src/test/java/no/nav/sbl/dialogarena/mininnboks/config/HenvendelseServiceMockContext.java
+++ web/src/test/java/no/nav/sbl/dialogarena/mininnboks/config/HenvendelseServiceMockContext.java
@@ -1,15 +1,15 @@
-package no.nav.sbl.dialogarena.mininnboks.consumer;
+package no.nav.sbl.dialogarena.mininnboks.config;
 
-import no.nav.sbl.dialogarena.mininnboks.config.MockApplicationContext;
+import no.nav.sbl.dialogarena.mininnboks.consumer.HenvendelseService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import static org.mockito.Mockito.mock;
 
 @Configuration
-public class HenvendelseServiceMockContext extends MockApplicationContext {
-
-    @Override
+@Import(MockApplicationContext.class)
+public class HenvendelseServiceMockContext {
     @Bean
     public HenvendelseService henvendelseService() {
         return mock(HenvendelseService.class);
