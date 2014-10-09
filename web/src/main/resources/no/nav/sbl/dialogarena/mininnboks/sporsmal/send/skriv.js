0180bdab3b2f6954da0db60c3a67c91fda7b17f0
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-10-09 10:45:53 +0200

    TG: Fjernet ubrukt javascript

--- web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/sporsmal/send/skriv.js
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/sporsmal/send/skriv.js
@@ -5,16 +5,3 @@ $(document).on('keydown', function(e) {
         $(".send-link").click();
     }
 });
-
-$(document).on('click', function () {
-    $('.endre-temagruppe-wrapper').hide();
-});
-
-$(document).on('click', '.send-panel .pil-ned, .send-panel .temagruppe-overskrift', function (e) {
-    $('.endre-temagruppe-wrapper').toggle();
-    e.stopPropagation();
-});
-
-$(document).on('click', '.endre-temagruppe', function (e) {
-    e.stopPropagation();
-});
