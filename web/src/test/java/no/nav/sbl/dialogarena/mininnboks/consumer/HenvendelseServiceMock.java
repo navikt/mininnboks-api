65cf1c306c8c72a6d4d2e95b3b9b9379cd76adb8
Nicklas Utgaard <nicklas.utgaard@nav.no>
2014-10-24 15:31:54 +0200

    PKFEIL-6068 Fiks i modig-wicket.

--- web/src/test/java/no/nav/sbl/dialogarena/mininnboks/consumer/HenvendelseServiceMock.java
+++ web/src/test/java/no/nav/sbl/dialogarena/mininnboks/consumer/HenvendelseServiceMock.java
@@ -103,13 +103,13 @@ public class HenvendelseServiceMock implements HenvendelseService {
         traad4Spsm.type = SPORSMAL_SKRIFTLIG;
         traad4Spsm.traadId = traad4Spsm.id;
         traad4Spsm.opprettet = now().minusHours(1);
-        traad4Spsm.fritekst = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. " +
+        traad4Spsm.fritekst = " https://navet.adeo.no/ansatt/Etatstjenester/Organisasjon+og+HR/Arbeidsmilj%C3%B8/Vil+lese+alle+svar.394779.cms Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. " +
                 "Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum" +
-                " iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto" +
+                " iriure dolor in hendrerit in vulputate velit esse molestie consequat, https://tjenester.nav.no/stillinger/stilling?sort=akt&s1=817395&rpp=50&p=0&ID=8641930&rv=al vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto" +
                 " odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend" +
                 " option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit" +
-                " eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui" +
-                " sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas";
+                " eorum claritatem. Investigationes demonstraverunt https://www.nav.no/no/Person/Hjelpemidler/Tjenester+og+produkter/Hjelpemidler/Om+hjelpemidler.358874.cms lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui" +
+                " sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam http://www.test.no/?id=1&id=2 nunc putamus parum claram, anteposuerit litterarum formas";
         traad4Spsm.temagruppe = FMLI;
         traad4Spsm.markerSomLest(traad4Spsm.opprettet);
         henvendelser.put(traad4Spsm.id, traad4Spsm);
