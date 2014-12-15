3d8f84713c5b854cdc2b589547ce74c9c0ec67c9
Henrik Hellerøy <henrik.helleroy@nav.no>
2014-12-15 10:03:52 +0100

    PK-14868: Kvittering tar mindre plass

--- web/src/main/java/no/nav/sbl/dialogarena/mininnboks/innboks/besvare/KvitteringPanel.java
+++ web/src/main/java/no/nav/sbl/dialogarena/mininnboks/innboks/besvare/KvitteringPanel.java
@@ -1,23 +1,12 @@
 package no.nav.sbl.dialogarena.mininnboks.innboks.besvare;
 
 import no.nav.sbl.dialogarena.mininnboks.panels.EpostPanel;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.event.Broadcast;
 import org.apache.wicket.markup.html.panel.Panel;
-
-import static no.nav.sbl.dialogarena.mininnboks.utils.Event.HENVENDELSE_BESVART;
 
 public class KvitteringPanel extends Panel {
     public KvitteringPanel(String id) {
         super(id);
 
         add(new EpostPanel("epostPanel"));
-        add(new AjaxLink("lukkKvittering") {
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-                send(getPage(), Broadcast.BREADTH, HENVENDELSE_BESVART);
-            }
-        });
     }
 }
