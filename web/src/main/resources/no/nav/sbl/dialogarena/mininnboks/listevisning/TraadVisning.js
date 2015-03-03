3820f313399719bb5facf40ed5b32fe0b025b9ff
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-03-03 16:27:36 +0100

    PK-12029: Indikasjon på antall meldinger i hver tråd

--- web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/listevisning/TraadVisning.js
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/listevisning/TraadVisning.js
@@ -7,13 +7,24 @@ var TraadVisning = React.createClass({
     render: function () {
         var melding = this.props.traad.nyesteHenvendelse;
         var dato = moment(melding.opprettet.millis).format('Do MMMM YYYY, [kl.] HH:mm');
+
         var lestImg = !melding.lest ? <img src="/mininnboks/img/melding_ny.svg" className="melding-info" /> : null;
+
+        var antall = this.props.traad.antallHenvendelser;
+        var antallImg = null;
+        if (antall == 2) {
+            antallImg = <img src="/mininnboks/img/melding_ett%20innlegg.svg" className="antall"/>
+        } else if (antall > 2) {
+            antallImg = <img src="/mininnboks/img/melding_flere%20innlegg.svg" className="antall"/>
+        }
+
         return (
             <a className="traadvisning" href={"/mininnboks/traad/" + melding.traadId}>
                 <h2>{dato}</h2>
             {lestImg}
                 <h3>{melding.statusTekst}</h3>
                 <p>{melding.fritekst}</p>
+            {antallImg}
             </a>
         )
     }
