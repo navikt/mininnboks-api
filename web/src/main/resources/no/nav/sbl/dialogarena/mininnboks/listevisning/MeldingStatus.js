4778e9063e6ef964af734e9fc04869280c8be382
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-03-05 15:14:44 +0100

    PK-12029: Endrer farge på bildene ved active og focus

--- web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/listevisning/MeldingStatus.js
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/listevisning/MeldingStatus.js
@@ -4,7 +4,7 @@ var MeldingStatus = React.createClass({
     render: function () {
         var melding = this.props.melding;
         if (!melding.lest) {
-            return (<img src="/mininnboks/img/melding_ny.svg" className="ikon melding-info" />)
+            return (<div className="ikon melding-info lest" />)
         } else if (melding.type === 'SVAR_SBL_INNGAAENDE') {
             return (<img src="/mininnboks/img/melding_besvart.svg" className="ikon melding-info" />)
         }
