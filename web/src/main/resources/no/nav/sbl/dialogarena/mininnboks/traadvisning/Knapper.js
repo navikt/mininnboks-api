d3cfa8a1a3ddbe929bb017724366ab6fea808e88
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-03-06 10:23:08 +0100

    PK-12029: Lagt til infoboks hvis man ikke kan besvare

--- web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/traadvisning/Knapper.js
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/traadvisning/Knapper.js
@@ -1,11 +1,16 @@
 var React = require('react');
 
 var Knapper = React.createClass({
-    render: function() {
+    render: function () {
+        var info =
+            <div className="info-boks">
+                <p>Denne samtalen er avsluttet og du kan ikke svare på meldingen. Hvis du vil stille et nytt spørsmål kan du starte en <a href="#">ny tråd</a></p>
+            </div>;
         return (
             <div className="knapper">
                 <a href="/mininnboks/reactinnboks" className="knapp-liten">Innboks</a>
                 <a href="#" className={'knapp-' + (this.props.kanBesvares ? 'hoved' : 'deaktivert') + '-liten'}>Skriv svar</a>
+                {this.props.kanBesvares ? null : info}
             </div>
         )
     }
