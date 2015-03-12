da487d3c783ef7358c2bd4a4aec8502c095668c2
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-03-12 12:25:34 +0100

    Nytt ikon for når bruker er avsender

--- web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/traadvisning/MeldingContainer.js
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/traadvisning/MeldingContainer.js
@@ -5,7 +5,7 @@ var MeldingContainer = React.createClass({
     render: function() {
         var melding = this.props.melding;
         var className = 'melding-container ' + (melding.fraBruker ? 'fra-bruker' : 'fra-nav');
-        var imgSrc = melding.fraBruker ? '/mininnboks/img/personligoppmote.svg' : '/mininnboks/img/nav-logo.svg';
+        var imgSrc = melding.fraBruker ? '/mininnboks/img/personikon.svg' : '/mininnboks/img/nav-logo.svg';
         return (
             <div className={className}>
                 <div className="logo"><img src={imgSrc} /></div>
