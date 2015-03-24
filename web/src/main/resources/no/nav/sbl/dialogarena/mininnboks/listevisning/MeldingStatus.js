3d1cab9b1209b7a34b8e705d9da7d4fff09e637d
Henrik Hellerøy <henrik.helleroy@nav.no>
2015-03-24 10:40:08 +0100

    Forenklet MeldingStatus-logikken

--- web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/listevisning/MeldingStatus.js
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/listevisning/MeldingStatus.js
@@ -6,19 +6,14 @@ var MeldingStatus = React.createClass({
         var status = 'ikon melding-info';
         var statusTekst = "Meldingen er";
 
-        if (melding.type === 'SPORSMAL_MODIA_UTGAAENDE') {
+        if (melding.type === 'SPORSMAL_MODIA_UTGAAENDE' || !melding.lest) {
             status += ' ubehandlet';
             statusTekst += ' ubehandlet.';
-        } else if(melding.type == 'SVAR_SBL_INNGAAENDE') {
+        } else if (melding.type == 'SVAR_SBL_INNGAAENDE') {
             status += ' besvart';
             statusTekst += ' besvart.';
         } else {
-            if(!melding.lest){
-                status += ' ubehandlet';
-                statusTekst += ' ubehandlet.';
-            } else {
-                return null;
-            }
+            return null;
         }
 
         return <div className={status} aria-label={statusTekst}/>;
