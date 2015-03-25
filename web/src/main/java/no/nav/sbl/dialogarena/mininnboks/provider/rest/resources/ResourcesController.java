f4e20c37765718f432a8aa27f947957b75cd5a3a
Nicklas Utgaard <nicklas.utgaard@nav.no>
2015-03-25 10:09:30 +0100

    Byttet ut AOP cache med enklere løsning som faktisk funker.

--- web/src/main/java/no/nav/sbl/dialogarena/mininnboks/provider/rest/resources/ResourcesController.java
+++ web/src/main/java/no/nav/sbl/dialogarena/mininnboks/provider/rest/resources/ResourcesController.java
@@ -4,38 +4,54 @@ import no.nav.modig.content.PropertyResolver;
 import no.nav.sbl.dialogarena.mininnboks.consumer.EpostService;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import java.util.HashMap;
 import java.util.Map;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @Path("/resources")
 @Produces(APPLICATION_JSON)
 public class ResourcesController {
 
+    public static final String EPOST = "bruker.epost";
     @Inject
     private EpostService epostService;
     @Inject
     private PropertyResolver propertyResolver;
 
     @GET
-    public Map<String, String> getResources() {
+    public Map<String, String> getResources(@Context HttpServletRequest request) {
+        String epost = epost(request);
         Map<String, String> resources = new HashMap<>();
 
         resources.putAll(propertyResolver.getAllProperties());
         resources.put("skriv.ny.link", System.getProperty("temavelger.link.url"));
         resources.put("brukerprofil.link", System.getProperty("brukerprofil.link.url"));
-        resources.put("bruker.epost", epost());
+        resources.put("bruker.epost", epost);
 
         return resources;
     }
 
-    private String epost() {
+    private String epost(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String epost = (String) session.getAttribute(EPOST);
+
+        if (!isBlank(epost)) {
+            return epost;
+        }
+
         try {
-            return epostService.hentEpostadresse();
+            epost =  epostService.hentEpostadresse();
+            session.setAttribute(EPOST, epost);
+
+            return epost;
         } catch (Exception e) {
             return "";
         }
