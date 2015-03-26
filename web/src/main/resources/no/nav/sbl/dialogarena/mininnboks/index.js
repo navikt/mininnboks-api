da2f6920e0762852f385885b1f933fccc274197e
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-03-26 10:31:25 +0100

    Forenklet gulpfilen til bare å kompilere index.js

---
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var MininnboksApp = React.createClass({
+    render: function () {
+        return (<h1>Hello, world!</h1>);
+    }
+});
+
+module.exports = MininnboksApp;
