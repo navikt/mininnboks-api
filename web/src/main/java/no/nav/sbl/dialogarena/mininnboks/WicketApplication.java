efb7567c219a3978f6f37d0ad80777aec8416bf7
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-03-30 11:50:22 +0200

    Tatt ut samtlige react-ressurser fra WicketApplication

--- web/src/main/java/no/nav/sbl/dialogarena/mininnboks/WicketApplication.java
+++ web/src/main/java/no/nav/sbl/dialogarena/mininnboks/WicketApplication.java
@@ -51,28 +51,9 @@ public class WicketApplication extends WebApplication {
         new FrontendConfigurator()
                 .withModules(EKSTERNFLATE)
                 .addMetas(CHARSET_UTF8, VIEWPORT_SCALE_1, XUA_IE_EDGE)
-                .addLess(
-                        new PackageResourceReference(WicketApplication.class, "melding/melding.less"),
-                        new PackageResourceReference(WicketApplication.class, "infoboks/infoboks.less"),
-                        new PackageResourceReference(WicketApplication.class, "expandingtextarea/expandingtextarea.less"),
-                        new PackageResourceReference(WicketApplication.class, "listevisning/listevisning.less"),
-                        new PackageResourceReference(WicketApplication.class, "traadvisning/traadvisning.less"),
-                        new PackageResourceReference(WicketApplication.class, "innboks.less"),
-                        new PackageResourceReference(SkrivPage.class, "sporsmal.less"))
+                .addLess(new PackageResourceReference(SkrivPage.class, "sporsmal.less"))
                 .withResourcePacking(this.usesDeploymentConfig())
-                .addScripts(
-                        SkrivPage.JQUERY_JS,
-                        new JavaScriptResourceReference(WicketApplication.class, "build/React.js"),
-                        new JavaScriptResourceReference(WicketApplication.class, "build/Utils.js"),
-                        new JavaScriptResourceReference(WicketApplication.class, "build/Snurrepipp.js"),
-                        new JavaScriptResourceReference(WicketApplication.class, "build/Feilmelding.js"),
-                        new JavaScriptResourceReference(WicketApplication.class, "build/Infoboks.js"),
-                        new JavaScriptResourceReference(WicketApplication.class, "build/Expandingtextarea.js"),
-                        new JavaScriptResourceReference(WicketApplication.class, "build/Melding.js"),
-                        new JavaScriptResourceReference(WicketApplication.class, "build/Resources.js"),
-                        new JavaScriptResourceReference(WicketApplication.class, "build/Listevisning.js"),
-                        new JavaScriptResourceReference(WicketApplication.class, "build/Traadvisning.js")
-                )
+                .addScripts(SkrivPage.JQUERY_JS)
                 .addCss(SkrivPage.JQUERY_CSS)
                 .configure(this);
         new ApplicationSettingsConfig().configure(this);
