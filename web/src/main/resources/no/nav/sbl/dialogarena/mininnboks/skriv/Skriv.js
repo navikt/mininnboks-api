597a2a20b5e6702c672328cc3b87b389190bd899
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-03-31 10:05:22 +0200

    Lagt til vilkårs-checkbox

--- web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/skriv/Skriv.js
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/skriv/Skriv.js
@@ -6,23 +6,26 @@ var Snurrepipp = require('../snurrepipp/Snurrepipp');
 var ExpandingTextArea = require('../expandingtextarea/ExpandingTextArea');
 
 var Skriv = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {hentet: false};
     },
-    componentDidMount: function() {
-        Utils.whenFinished([Resources.promise]).then(function() {
+    componentDidMount: function () {
+        Utils.whenFinished([Resources.promise]).then(function () {
             this.setState({hentet: true})
         }.bind(this))
     },
-    render: function() {
+    render: function () {
         return this.state.hentet ? (
             <div className="innboks-container">
                 <h1 className="diger">{Resources.get('send-sporsmal.still-sporsmal.ny-melding-overskrift')}</h1>
                 <article className="send-sporsmal-container send-panel">
                     <form>
                         <div className="sporsmal-header">
-                            <img src="/mininnboks/img/melding_graa.svg" alt={Resources.get("meldingikon.alternativ.tekst")}/>
+                            <img src="/mininnboks/img/melding_graa.svg"
+                                 alt={Resources.get("meldingikon.alternativ.tekst")}/>
+
                             <h2 className="stor deloverskrift">{Resources.get("send-sporsmal.still-sporsmal.deloverskrift")}</h2>
+
                             <div className="robust-strek"></div>
                         </div>
 
@@ -30,9 +33,22 @@ var Skriv = React.createClass({
 
 
                         <p className="hjelpetekst">{Resources.get("send-sporsmal.still-sporsmal.hjelpetekst")}</p>
-                        <ExpandingTextArea placeholder={Resources.get('traadvisning.besvar.tekstfelt.placeholder')} charsLeftText={Resources.get('traadvisning.besvar.tekstfelt.tegnigjen')} ref="textarea" />
-                        <div><input type="submit" className="send-link knapp-hoved-stor" role="button" value={Resources.get("send-sporsmal.still-sporsmal.send-inn")} /></div>
-                        <div className="avbryt"><Link to="innboks">{Resources.get("send-sporsmal.still-sporsmal.avbryt")}</Link></div>
+                        <ExpandingTextArea placeholder={Resources.get('skriv-sporsmal.fritekst.placeholder')}
+                                           charsLeftText={Resources.get('traadvisning.besvar.tekstfelt.tegnigjen')}
+                                           ref="textarea"/>
+
+                        <div className="betingelsevalgpanel">
+                            <div className="checkbox">
+                                <input type="checkbox" name="betingelseValg:betingelserCheckbox" id="betingelser"/>
+                                <label htmlFor="betingelser"><span>{Resources.get("send-sporsmal.still-sporsmal.betingelser.sjekkboks")}</span></label>
+                                <a href="#" className="vilkarlenke">{Resources.get("send-sporsmal.still-sporsmal.betingelser.vis")}</a>
+                            </div>
+                        </div>
+
+                        <div><input type="submit" className="send-link knapp-hoved-stor" role="button"
+                                    value={Resources.get("send-sporsmal.still-sporsmal.send-inn")}/></div>
+                        <div className="avbryt"><Link
+                            to="innboks">{Resources.get("send-sporsmal.still-sporsmal.avbryt")}</Link></div>
                     </form>
                 </article>
             </div>
