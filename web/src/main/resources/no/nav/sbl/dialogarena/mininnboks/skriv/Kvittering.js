ef0eedd8504a198c174362b67b2169dcd7286cb5
Frithjof Nerdrum <frithjof.nerdrum@nav.no>
2015-04-01 14:48:16 +0200

    Viser kvittering etter å ha sendt spørsmål

---
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/skriv/Kvittering.js
@@ -0,0 +1,32 @@
+var React = require('react');
+var Link = require('react-router').Link;
+
+var Kvittering = React.createClass({
+    render: function () {
+        return (
+            <article className="send-sporsmal-container bekreftelse-panel">
+                <div className="sporsmal-header">
+                    <img src="/mininnboks/img/hake_stor02.svg" alt="Kvitteringsikon"/>
+
+                    <h2 className="stor deloverskrift">{this.props.resources.get('send-sporsmal.bekreftelse.antall-dager')}</h2>
+
+                    <div className="robust-strek"></div>
+
+
+                    <div className="mangler-epost">
+                        {this.props.resources.get('send-sporsmal.bekreftelse.du-kan-motta-epost')}
+                        <a className="brukerprofil-link" href={this.props.resources.get('brukerprofil.link')}>
+                            {this.props.resources.get('send-sporsmal.bekreftelse.registrer-epostadresse')}
+                        </a>
+                    </div>
+
+
+                    <hr/>
+                </div>
+                <Link className="knapp-link-stor" to="innboks">{this.props.resources.get('send-sporsmal.bekreftelse.til-meldingsboksen')}</Link>
+            </article>
+        );
+    }
+});
+
+module.exports = Kvittering;
--- web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/skriv/Skriv.js
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/skriv/Skriv.js
@@ -3,12 +3,17 @@ var Link = require('react-router').Link;
 var ExpandingTextArea = require('../expandingtextarea/ExpandingTextArea');
 var FeedbackForm = require('../feedback/FeedbackForm');
 var GodtaVilkar = require('./GodtaVilkar');
+var Kvittering = require('./Kvittering');
 
 var Skriv = React.createClass({
-    onSubmit: function() {
+    getInitialState: function () {
+        return {sender: false, sendt: false};
+    },
+    onSubmit: function () {
         var form = this.refs.form;
         form.validate();
         if (form.isValid()) {
+            this.setState({sender: true});
             var temagruppe = this.props.params.temagruppe;
             var fritekst = form.getFeedbackRef('textarea').getInput();
             $.ajax({
@@ -16,44 +21,51 @@ var Skriv = React.createClass({
                 url: '/mininnboks/tjenester/traader/sporsmal',
                 contentType: 'application/json',
                 data: JSON.stringify({temagruppe: temagruppe, fritekst: fritekst})
-            }).done(function() {console.log('sendt')});
+            }).done(function () {
+                this.setState({sender: false, sendt: true});
+            }.bind(this));
         }
     },
     render: function () {
-        return (
-            <div className="innboks-container">
-                <h1 className="diger">{this.props.resources.get('send-sporsmal.still-sporsmal.ny-melding-overskrift')}</h1>
-                <article className="send-sporsmal-container send-panel">
-                    <div className="sporsmal-header">
-                        <img src="/mininnboks/img/melding_graa.svg"
-                             alt={this.props.resources.get("meldingikon.alternativ.tekst")}/>
+        if (this.state.sendt) {
+            return <Kvittering resources={this.props.resources} />
+        } else {
+            return (
+                <div className="innboks-container">
+                    <h1 className="diger">{this.props.resources.get('send-sporsmal.still-sporsmal.ny-melding-overskrift')}</h1>
+                    <article className="send-sporsmal-container send-panel">
+                        <div className="sporsmal-header">
+                            <img src="/mininnboks/img/melding_graa.svg"
+                                 alt={this.props.resources.get("meldingikon.alternativ.tekst")}/>
 
-                        <h2 className="stor deloverskrift">{this.props.resources.get("send-sporsmal.still-sporsmal.deloverskrift")}</h2>
+                            <h2 className="stor deloverskrift">{this.props.resources.get("send-sporsmal.still-sporsmal.deloverskrift")}</h2>
 
-                        <div className="robust-strek"></div>
-                    </div>
-
-                    <strong>{this.props.resources.get(this.props.params.temagruppe)}</strong>
-
-                    <p className="hjelpetekst">{this.props.resources.get("send-sporsmal.still-sporsmal.hjelpetekst")}</p>
-                    <FeedbackForm ref="form">
-                        <ExpandingTextArea placeholder={this.props.resources.get('skriv-sporsmal.fritekst.placeholder')}
-                                           charsLeftText={this.props.resources.get('traadvisning.besvar.tekstfelt.tegnigjen')}
-                                           feedbackref="textarea"/>
-
-                        <GodtaVilkar />
-
-                        <div>
-                            <input type="button" className="send-link knapp-hoved-stor" role="button"
-                                   value={this.props.resources.get("send-sporsmal.still-sporsmal.send-inn")} onClick={this.onSubmit} />
+                            <div className="robust-strek"></div>
                         </div>
-                        <div className="avbryt">
-                            <Link to="innboks">{this.props.resources.get("send-sporsmal.still-sporsmal.avbryt")}</Link>
-                        </div>
-                    </FeedbackForm>
-                </article>
-            </div>
-        );
+
+                        <strong>{this.props.resources.get(this.props.params.temagruppe)}</strong>
+
+                        <p className="hjelpetekst">{this.props.resources.get("send-sporsmal.still-sporsmal.hjelpetekst")}</p>
+                        <FeedbackForm ref="form">
+                            <ExpandingTextArea placeholder={this.props.resources.get('skriv-sporsmal.fritekst.placeholder')}
+                                               charsLeftText={this.props.resources.get('traadvisning.besvar.tekstfelt.tegnigjen')}
+                                               feedbackref="textarea"/>
+
+                            <GodtaVilkar />
+
+                            <div>
+                                <input type="button" className="send-link knapp-hoved-stor" role="button"
+                                       value={this.props.resources.get("send-sporsmal.still-sporsmal.send-inn")}
+                                       onClick={this.onSubmit}/>
+                            </div>
+                            <div className="avbryt">
+                                <Link to="innboks">{this.props.resources.get("send-sporsmal.still-sporsmal.avbryt")}</Link>
+                            </div>
+                        </FeedbackForm>
+                    </article>
+                </div>
+            );
+        }
     }
 });
 
