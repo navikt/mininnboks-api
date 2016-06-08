fc1e82b1c9a5e9463d96706996c7702635804e2d
Andreas Lund <andreas.lund@nav.no>
2016-06-08 14:44:17 +0200

    Fikser UU-issues.

--- web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/expandingtextarea/ExpandingTextArea.js
+++ web/src/main/resources/no/nav/sbl/dialogarena/mininnboks/expandingtextarea/ExpandingTextArea.js
@@ -10,7 +10,12 @@ class ExpandingTextArea extends React.Component {
     render() {
         const { formatMessage, dispatch, fritekst, validationResult } = this.props;
         const resterendeLengde = 1000 - fritekst.length;
-        const additionalClassName = validationResult.includes(FeilmeldingEnum.textarea) ? 'invalid' : '';
+        const hasValidationError = validationResult.includes(FeilmeldingEnum.textarea);
+        const additionalClassName = hasValidationError ? 'invalid' : '';
+        const feilmelding = hasValidationError
+            ? <span className="skjema-feilmelding" id="textarea.feilmelding" role="alert" aria-live="assertive" aria-atomic="true">{formatMessage({ id: 'feilmeldingliste.textarea'}) }</span>
+            : <noscript/>;
+        const ariadescribedby = hasValidationError ?  'textarea.feilmelding' : '';
 
         return (
             <div className="textarea-meta-container js-container">
@@ -20,14 +25,18 @@ class ExpandingTextArea extends React.Component {
                 <SamletFeilmeldingPanel formatMessage={formatMessage} validationResult={validationResult}/>
 
                 <textarea id="textarea-med-meta" name="textarea-med-meta" className={`input-fullbredde typo-normal ${additionalClassName}`}
-                  title={ formatMessage({ id: 'traadvisning.besvar.tekstfelt' }) }
-                  aria-label={ formatMessage({ id: 'traadvisning.besvar.tekstfelt' }) } aria-invalid="false" aria-describedby=''
-                  onChange={ _onWrite(dispatch)}
-                  value={fritekst}
+                          autofocus
+                          title = { formatMessage({ id: 'traadvisning.besvar.tekstfelt' }) }
+                          aria-label = { formatMessage({ id: 'traadvisning.besvar.tekstfelt' }) }
+                          aria-invalid = { hasValidationError }
+                          aria-describedby = { ariadescribedby }
+                          onChange = { _onWrite(dispatch)}
+                          value = { fritekst }
                 />
                 <p className="textarea-metatekst" aria-hidden="true">
                  <span class="max-length">{resterendeLengde}</span> tegn igjen
                 </p>
+                {feilmelding}
             </div>
         );
     }
@@ -42,4 +51,3 @@ ExpandingTextArea.propTypes = {
 };
 
 export default connect()(ExpandingTextArea);
-
