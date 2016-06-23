31c64cf916d969ed70bf588f9807745f5d3b5484
Aleksander Hindenes <aleksander.hindenes@nav.no>
2016-06-23 15:30:52 +0200

    Sonar: Sonar liker fortsatt ikke metode-referanser

--- web/src/main/java/no/nav/sbl/dialogarena/mininnboks/consumer/utils/HenvendelsesUtils.java
+++ web/src/main/java/no/nav/sbl/dialogarena/mininnboks/consumer/utils/HenvendelsesUtils.java
@@ -28,7 +28,7 @@ public abstract class HenvendelsesUtils {
     private static final String LINE_BREAK = "\n";
 
     public static final List<Henvendelsetype> FRA_BRUKER = asList(SPORSMAL_SKRIFTLIG, SVAR_SBL_INNGAAENDE);
-    public static final Function<XMLHenvendelse, Henvendelse> TIL_HENVENDELSE = HenvendelsesUtils::tilHenvendelse;
+    public static final Function<XMLHenvendelse, Henvendelse> TIL_HENVENDELSE = xmlHenvendelse -> tilHenvendelse(xmlHenvendelse);
 
     public static void setCmsContentRetriever(CmsContentRetriever contentRetriever) {
         cmsContentRetriever = contentRetriever;
