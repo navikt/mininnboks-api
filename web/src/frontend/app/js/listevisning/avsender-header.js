7be3948c0da50d89f05b38d9da7bce09265cfdcb
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-07-19 16:38:12 +0200

    Bruker skikkelig funksjon definisjon

--- web/src/frontend/app/js/listevisning/avsender-header.js
+++ web/src/frontend/app/js/listevisning/avsender-header.js
@@ -1,14 +1,14 @@
 import React, { PropTypes as pt } from 'react';
 import { FormattedMessage } from 'react-intl';
 
-const AvsenderHeader = ({ meldingType }) => {
+function AvsenderHeader({ meldingType }) {
     const avsender = <span className="avsender-fra-nav"><FormattedMessage id="avsender.tekst.NAV" /></span>;
 
     const maaBehandlesStatus = meldingType === 'SPORSMAL_MODIA_UTGAAENDE' ?
         <span>/ <strong className="purring"><FormattedMessage id="purre.svar" /></strong></span> : null;
 
     return <span>/ Fra {avsender} {maaBehandlesStatus}</span>;
-};
+}
 
 AvsenderHeader.propTypes = {
     meldingType: pt.string.isRequired
