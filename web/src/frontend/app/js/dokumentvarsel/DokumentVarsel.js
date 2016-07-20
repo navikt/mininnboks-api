28b1180f5a6f4f750ea1b357d4600f4e4c2d100a
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-07-20 09:56:26 +0200

    Renskriving dokumentvarsel

--- web/src/frontend/app/js/dokumentvarsel/DokumentVarsel.js
+++ web/src/frontend/app/js/dokumentvarsel/DokumentVarsel.js
@@ -2,32 +2,31 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { lesDokumentVarsel } from '../utils/actions/actions';
 import { hentDokumentVisningData } from './varsel-actions';
-import { injectIntl, intlShape } from 'react-intl';
 import Dokumentvinsing from './dokumentvinsing';
 import Breadcrumbs from '../utils/brodsmulesti/customBreadcrumbs';
 
 class DokumentVarsel extends React.Component {
     componentDidMount() {
-        const { dispatch, params, traader } = this.props;
-        const traad = traader.filter( (traad) => traad.traadId === params.id)[0];
+        const { params, traader, lesDokumentVarsel, hentDokumentVisningData } = this.props;
+        const traad = traader.find( (traad) => traad.traadId === params.id);
         if(traad && !traad.meldinger[0].lest) {
-            dispatch(lesDokumentVarsel(params.id));
+            lesDokumentVarsel(params.id);
         }
         if(traad && traad.meldinger[0]) {
             const varsel = traad.meldinger[0];
-            dispatch(hentDokumentVisningData(varsel.journalpostId, varsel.dokumentIdListe.join('-')));
+            hentDokumentVisningData(varsel.journalpostId, varsel.dokumentIdListe.join('-'));
         }
     }
 
     render() {
         if(!this.props.dokumentvisning) {
-            return <noscript/>;
+            return null;
         }
-        const { params, routes, intl } = this.props;
+        const { params, routes } = this.props;
 
         return (
             <div className="dokinnsyn">
-                <Breadcrumbs routes={routes} params={params} formatMessage={intl.formatMessage}/>
+                <Breadcrumbs routes={routes} params={params} />
                 <Dokumentvinsing { ...this.props.dokumentvisning } />
             </div>
         );
@@ -35,5 +34,9 @@ class DokumentVarsel extends React.Component {
 }
 
 const mapStateToProps = ({ traader, dokumentvisning  }) => ({ traader, dokumentvisning });
+const mapDispatchToProps = (dispatch) => ({
+    lesDokumentVarsel: (id) => dispatch(lesDokumentVarsel(id)),
+    hentDokumentVisningData: (journalpostId, idListe) => dispatch(hentDokumentVisningData(journalpostId, idListe))
+});
 
-export default injectIntl(connect(mapStateToProps)(DokumentVarsel));
+export default connect(mapStateToProps, mapDispatchToProps)(DokumentVarsel);
