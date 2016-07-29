22a540460e71328485f1e4010a001b291f37eb8b
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-07-29 09:17:03 +0200

    La inn igjen auto-expanding textarea

--- web/src/frontend/app/expanding-textarea/expanding-textarea.js
+++ web/src/frontend/app/expanding-textarea/expanding-textarea.js
@@ -1,65 +1,76 @@
-import React, { PropTypes as PT } from 'react';
+import React, { Component, PropTypes as PT } from 'react';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import classNames from 'classnames';
-import { reduxFormProps } from './../utils/utils';
+import { reduxFormProps, autobind } from './../utils/utils';
+import InlineFeilmelding from './../utils/nav-form/inline-feilmelding';
 
-function ExpandingTextArea({ intl, makslengde, config, feilmeldingpanel }) {
-    const resterendeLengde = makslengde - config.value.length;
-    const showError = config.error && config.touched;
+class ExpandingTextArea extends Component {
+    constructor(props) {
+        super(props);
+        autobind(this);
+    }
 
-    const textareaClassname = classNames('input-fullbredde typo-normal', {
-        invalid: showError
-    });
-    const ariadescribedby = showError ? intl.formatMessage({ id: 'textarea.feilmelding' }) : '';
-    const title = intl.formatMessage({ id: 'traadvisning.besvar.tekstfelt' });
-    const ariaLabel = intl.formatMessage({ id: 'traadvisning.besvar.tekstfelt' });
-    const feilmelding = showError ? (
-        <span
-            className="skjema-feilmelding"
-            id="textarea.feilmelding"
-            role="alert"
-            aria-live="assertive"
-            aria-atomic="true"
-        >
-            <FormattedMessage id={`feilmelding.fritekst.${config.error}`} />
-        </span>
-    ) : null;
+    onChangeProxy(e) {
+        this.refs.mirror.textContent = e.target.value;
+        this.refs.textarea.style.height = `${this.refs.mirror.offsetHeight}px`;
+        this.props.config.onChange(e);
+    }
 
-    /* eslint-disable jsx-a11y/no-onchange */
-    return (
-        <div className="textarea-meta-container js-container">
-            <label htmlFor="fritekst">
-                <span className="typo-normal max-length"><FormattedMessage id="textarea.infotekst" /></span>
-            </label>
-            {feilmeldingpanel}
-            <textarea
-                id="fritekst"
-                name="fritekst"
-                className={textareaClassname}
-                autoFocus
-                title={title}
-                aria-label={ariaLabel}
-                aria-invalid={showError}
-                aria-describedby={ariadescribedby}
-                {...reduxFormProps(config)}
-            />
-            <p className="textarea-metatekst" aria-hidden="true">
-                <span className="max-length">{resterendeLengde}</span> tegn igjen
-            </p>
-            {feilmelding}
-        </div>
-    );
+    render() {
+        const { intl, makslengde, config } = this.props;
+        const resterendeLengde = makslengde - config.value.length;
+        const skalViseFeilmelding = !!(config.error && config.touched);
+
+        const textareaClassname = classNames('textfelt input-fullbredde', { invalid: skalViseFeilmelding });
+        const containerClassname = classNames('textarea-meta-container js-container', {
+            'har-valideringsfeil': skalViseFeilmelding
+        });
+
+        const ariadescribedby = skalViseFeilmelding ? 'textarea.feilmelding' : '';
+        const title = intl.formatMessage({ id: 'traadvisning.besvar.tekstfelt' });
+        const ariaLabel = intl.formatMessage({ id: 'traadvisning.besvar.tekstfelt' });
+
+        /* eslint-disable jsx-a11y/no-onchange */
+        return (
+            <div className="expanding-textarea">
+                <div className="textareamirror" ref="mirror" aria-hidden="true"></div>
+                <div className={containerClassname}>
+                    <label htmlFor="fritekst">
+                    <span className="typo-normal max-length vekk">
+                        <FormattedMessage id="skriv.fritekst.label" values={{ antallTegn: makslengde }}/>
+                    </span>
+                    </label>
+                    <textarea
+                        id="fritekst"
+                        name="fritekst"
+                        ref="textarea"
+                        className={textareaClassname}
+                        autoFocus
+                        title={title}
+                        aria-label={ariaLabel}
+                        aria-invalid={skalViseFeilmelding}
+                        aria-describedby={ariadescribedby}
+                        {...reduxFormProps({ ...config, onChange: this.onChangeProxy })}
+                    />
+                    <p className="textarea-metatekst" aria-hidden="true">
+                        <span className="max-length">{resterendeLengde}</span> tegn igjen
+                    </p>
+                </div>
+                <InlineFeilmelding id="textarea.feilmelding" visibleIf={skalViseFeilmelding}>
+                    <FormattedMessage id={`feilmelding.fritekst.${config.error}`} />
+                </InlineFeilmelding>
+            </div>
+        );
+    }
 }
 
 ExpandingTextArea.defaultProps = {
-    makslengde: 1000,
-    feilmeldingpanel: null
+    makslengde: 1000
 };
 ExpandingTextArea.propTypes = {
     intl: PT.object.isRequired,
     config: PT.object.isRequired,
-    makslengde: PT.number,
-    feilmeldingpanel: PT.node
+    makslengde: PT.number
 };
 
 export default injectIntl(ExpandingTextArea);
