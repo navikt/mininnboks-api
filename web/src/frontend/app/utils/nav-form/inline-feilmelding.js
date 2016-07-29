31824e3e27243ba0e765ac4348363d15b162bad4
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-07-29 09:17:18 +0200

    Dro ut inlinefeilmelding til egen komponent

---
+++ web/src/frontend/app/utils/nav-form/inline-feilmelding.js
@@ -0,0 +1,23 @@
+import React, { PropTypes as PT } from 'react';
+import { visibleIf } from './../hocs/visible-if';
+
+function InlineFeilmelding({ id, children }) {
+    return (
+        <span
+            id={id}
+            className="skjema-feilmelding"
+            role="alert"
+            aria-live="assertive"
+            aria-atomic="true"
+        >
+            {children}
+        </span>
+    );
+}
+
+InlineFeilmelding.propTypes = {
+    id: PT.string.isRequired,
+    children: PT.node.isRequired
+};
+
+export default visibleIf(InlineFeilmelding);
