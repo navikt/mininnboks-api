a417aaa99272686822c4a29c3388e4d593000a22
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-08-25 08:33:33 +0200

    La til header (Personalia)

--- web/src/frontend/app/dokument-visning/dokument-visning.js
+++ web/src/frontend/app/dokument-visning/dokument-visning.js
@@ -1,5 +1,6 @@
 import React, { PropTypes as PT } from 'react';
-import { Dokumenter, Hurtignavigering } from 'react-dokumentvisning';
+import { Personalia, Dokumenter, Hurtignavigering } from 'react-dokumentvisning';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import FixedPosition from './../utils/fixed-position';
 import Breadcrumbs from '../brodsmulesti/custom-breadcrumbs';
 
@@ -12,11 +13,26 @@ class DokumentVisning extends React.Component {
     }
 
     render() {
-        const { dokumentmetadata, journalpostmetadata, routes, params } = this.props;
+        const { dokumentmetadata, journalpostmetadata, intl, routes, params } = this.props;
+        const { temakode } = journalpostmetadata.resultat;
+        const kontaktNavUrl = intl.messages['dokumentvisning.kontakt.nav.link'];
 
         return (
             <div>
                 <Breadcrumbs routes={routes} params={params} />
+                <section className="dokumentvisning-header blokk-m">
+                    <Personalia journalpostmetadata={journalpostmetadata.resultat} hode={false} />
+                    <ul className="ustilet">
+                        <li>
+                            <a href={`/saksoversikt/app/tema/${temakode}`}>
+                                <FormattedMessage id="dokumentvisning.gatil.saksoversikt"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href={kontaktNavUrl}><FormattedMessage id="dokumentvisning.kontakt.nav"/></a>
+                        </li>
+                    </ul>
+                </section>
                 <section className="dokumenter">
                     <FixedPosition>
                         <Hurtignavigering dokumentmetadata={dokumentmetadata} navigeringsknappOffset={offset} />
@@ -35,7 +51,8 @@ DokumentVisning.propTypes = {
     dokumentmetadata: PT.array.isRequired,
     journalpostmetadata: PT.object.isRequired,
     params: PT.object.isRequired,
-    routes: PT.array.isRequired
+    routes: PT.array.isRequired,
+    intl: PT.object.isRequired
 };
 
-export default DokumentVisning;
+export default injectIntl(DokumentVisning);
