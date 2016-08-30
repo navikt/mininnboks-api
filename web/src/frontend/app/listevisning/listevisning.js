9a34435e63d21d1b4e107350e69ca07582d2f0a2
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-08-30 11:54:11 +0200

    Fjerner markering på første element som default. Markerer kun ved matchende varselId == korelasjonsId

--- web/src/frontend/app/listevisning/listevisning.js
+++ web/src/frontend/app/listevisning/listevisning.js
@@ -20,26 +20,20 @@ const getTraadLister = (traader) => {
         leste
     };
 };
-const erAktivRegel = (fantVarselId, varselId) => {
-    if (!fantVarselId) {
-        return (_, index) => index === 0;
-    }
-    return (melding) => melding.korrelasjonsId === varselId;
-};
 
+const erAktivRegel = (varselId) => (melding) => melding.korrelasjonsId === varselId;
 
 function ListeVisning({ routes, params, traader, location }) {
     const varselId = location.query.varselId;
     const traaderGruppert = getTraadLister(traader.data);
 
-    const fantVarselId = traader.data.find((traad) => traad.nyeste.korrelasjonsId === varselId);
-    const erAktiv = erAktivRegel(fantVarselId, varselId);
+    const erAktiv = erAktivRegel(varselId);
 
     const ulesteTraader = traaderGruppert.uleste.map((traad, index) => ({
-        traad, aktiv: erAktiv(traad.nyeste, index), ulestMeldingKlasse: 'uleste-meldinger'
+        traad, aktiv: erAktiv(traad.nyeste), ulestMeldingKlasse: 'uleste-meldinger'
     }));
     const lesteTraader = traaderGruppert.leste.map((traad, index) => ({
-        traad, aktiv: erAktiv(traad.nyeste, index + ulesteTraader.length)
+        traad, aktiv: erAktiv(traad.nyeste)
     }));
 
     return (
