5ef4fb3eceb6e6b402d73d6816d9fec18e91e16d
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-09-14 15:30:23 +0200

    Fikset småbugs

--- web/src/frontend/app/innholdslaster/innholdslaster-laster.js
+++ web/src/frontend/app/innholdslaster/innholdslaster-laster.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Spinner from 'nav-react-design/dist/spinner';
+import { FormattedMessage } from 'react-intl';
 
 function Laster() {
     return (
         <div className="innholdslaster">
-            <div className="spinner spinner-xxl"></div>
+            <FormattedMessage id="spinner">
+                {(label) => <Spinner storrelse="xxl" aria-label={label}/>}
+            </FormattedMessage>
         </div>
     );
 }
