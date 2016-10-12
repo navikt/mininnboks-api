36c15e469366ac8e6c8d6cb5118b929be813766b
Christian Finstad <christian.finstad@nav.no>
2016-10-12 09:30:18 +0200

    eslint

--- web/src/frontend/app/ducks/traader-test.js
+++ web/src/frontend/app/ducks/traader-test.js
@@ -1,24 +1,9 @@
 /* eslint-env mocha */
 import reducer, * as E from './traader';
-import { expect, Assertion } from 'chai';
-import configureMockStore from 'redux-mock-store';
-import { STATUS } from './utils'
+import { expect } from 'chai';
 
 describe('traader-ducks', () => {
     describe('reducer', () => {
-
-        function erAlleMeldingerLest(traadId, markertSomLest) {
-            return markertSomLest.data
-                .filter((traad) => (traad.traadId === traadId))
-                .map( (traad) => (traad.meldinger))
-                .filter(harUlestMelding);
-        }
-
-        function harUlestMelding(meldinger) {
-            const lestArray = meldinger.filter( (melding) => (melding.lest) );
-            return lestArray[0] == undefined;
-        }
-
         it('skal oppdatere alle meldinger med med rett traadId med status lest', () => {
             const messagesTraad1 = [
                 {
@@ -44,12 +29,12 @@ describe('traader-ducks', () => {
                 }];
 
             const initialState = {
-                data: [{traadId: 'traad1',
+                data: [{ traadId: 'traad1',
                         meldinger: messagesTraad1,
                         nyeste: messagesTraad1[0],
                         eldste: messagesTraad1[1]
                         },
-                    {traadId: 'traad2',
+                    { traadId: 'traad2',
                         meldinger: messagesTraad2,
                         nyeste: messagesTraad2[0],
                         eldste: messagesTraad2[1]
@@ -62,10 +47,21 @@ describe('traader-ducks', () => {
                 }
             });
 
-            expect(erAlleMeldingerLest('traad1', markertSomLest)).to.deep.equal([]);
-            expect(erAlleMeldingerLest('traad2',markertSomLest)).to.not.deep.equal([]);
+            function harUlestMelding(meldinger) {
+                const lestArray = meldinger.filter((melding) => (melding.lest));
+                return lestArray[0] === undefined;
+            }
+
+            function erAlleMeldingerLest(traadId) {
+                return markertSomLest.data
+                    .filter((traad) => (traad.traadId === traadId))
+                    .map((traad) => (traad.meldinger))
+                    .filter(harUlestMelding);
+            }
+
+            expect(erAlleMeldingerLest('traad1')).to.deep.equal([]);
+            expect(erAlleMeldingerLest('traad2')).to.not.deep.equal([]);
         });
     });
 });
-
 
--- web/src/frontend/app/ducks/utils-test.js
+++ web/src/frontend/app/ducks/utils-test.js
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
-import { expect, Assertion } from 'chai';
-import { sjekkStatuskode, toJson, getCookie } from "./utils.js";
+import { expect } from 'chai';
+import { sjekkStatuskode, toJson, getCookie } from './utils.js';
 
 describe('utils', () => {
     describe('Sjekk-statuskode', () => {
@@ -9,7 +9,7 @@ describe('utils', () => {
                 ok: true,
                 status: 200,
                 statusText: 'Status OK'
-            }
+            };
             expect(sjekkStatuskode(response)).to.deep.equal(response);
         });
         it('Skal returnere error når respons ikke er ok', () => {
@@ -17,60 +17,59 @@ describe('utils', () => {
                 ok: false,
                 status: 200,
                 statusText: 'Feilstatus'
-            }
-            expect( () => sjekkStatuskode(response)).to.throw(Error)
+            };
+            expect(() => sjekkStatuskode(response)).to.throw(Error);
         });
         it('Skal returnere error når status er over 299', () => {
             const response = {
                 ok: true,
                 status: 300,
                 statusText: 'Feilstatus'
-            }
-            expect( () => sjekkStatuskode(response)).to.throw(Error)
+            };
+            expect(() => sjekkStatuskode(response)).to.throw(Error);
         });
         it('Skal returnere error når status er under 200', () => {
             const response = {
                 ok: true,
                 status: 199,
                 statusText: 'Feilstatus'
-            }
-            expect( () => sjekkStatuskode(response)).to.throw(Error)
+            };
+            expect(() => sjekkStatuskode(response)).to.throw(Error);
         });
         it('Skal returnere error når statuskode er under 200 og ok er false', () => {
             const response = {
                 ok: false,
                 status: 199,
                 statusText: 'Feilstatus'
-            }
-            expect( () => sjekkStatuskode(response)).to.throw(Error)
+            };
+            expect(() => sjekkStatuskode(response)).to.throw(Error);
         });
     });
     describe('toJson', () => {
         it('Sjekk at funksjonen returnere json ved gyldig status', () => {
             const response = {
                 status: 200,
-                json: () =>  { return {testprop:'testprop'}}
-            }
-            expect( toJson(response)).to.deep.equal(response.json())
+                json: () => ({ testprop: 'testprop' })
+            };
+            expect(toJson(response)).to.deep.equal(response.json());
         });
         it('Returnerer respons ved 204', () => {
             const response = {
                 status: 204,
-                json: () =>  { return {testprop:'testprop'}}
-            }
-            expect( toJson(response)).to.deep.equal(response)
+                json: () => ({ testprop: 'testprop' })
+            };
+            expect(toJson(response)).to.deep.equal(response);
         });
     });
     describe('getCookie', () => {
         it('Henter ut fra cookie', () => {
-            global.document.cookie = "test1=detteerentest123; test2=detteerogsåentest123";
+            global.document.cookie = 'test1=detteerentest123; test2=detteerogsåentest123';
             expect(getCookie('test1')).to.equal('detteerentest123');
         });
         it('Tom streng ved inge match', () => {
-            global.document.cookie = "test1=detteerentest123; test2=detteerogsåentest123";
+            global.document.cookie = 'test1=detteerentest123; test2=detteerogsåentest123';
             expect(getCookie('test0')).to.equal('');
-        })
+        });
     });
 });
-
 
