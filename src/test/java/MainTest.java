937912ec8efc6d5be5281a88d004a52b3ad47af2
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-12-21 10:07:37 +0100

    naisifiserer og api-appifiserer apiet til mininnboks

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,43 @@
+import no.nav.dialogarena.config.fasit.FasitUtils;
+import no.nav.dialogarena.config.fasit.ServiceUser;
+import no.nav.testconfig.ApiAppTest;
+
+import static no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig.AZUREAD_B2C_DISCOVERY_URL_PROPERTY_NAME;
+import static no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig.AZUREAD_B2C_EXPECTED_AUDIENCE_PROPERTY_NAME;
+import static no.nav.dialogarena.config.fasit.FasitUtils.Zone.SBS;
+import static no.nav.dialogarena.config.fasit.FasitUtils.getBaseUrl;
+import static no.nav.dialogarena.config.fasit.FasitUtils.getServiceUser;
+import static no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants.*;
+import static no.nav.sbl.dialogarena.mininnboks.config.ServiceConfig.*;
+import static no.nav.sbl.util.EnvironmentUtils.Type.PUBLIC;
+import static no.nav.sbl.util.EnvironmentUtils.Type.SECRET;
+import static no.nav.sbl.util.EnvironmentUtils.setProperty;
+
+public class MainTest {
+
+    private static final String APPLICATION_NAME = "mininnboks-api";
+
+    public static void main(String[] args) {
+        ApiAppTest.setupTestContext(ApiAppTest.Config.builder().applicationName(APPLICATION_NAME).build());
+
+        ServiceUser serviceUser = getServiceUser("srvmininnboks", "mininnboks", SBS);
+        setProperty(SYSTEMUSER_USERNAME, serviceUser.getUsername(), PUBLIC);
+        setProperty(SYSTEMUSER_PASSWORD, serviceUser.getPassword(), SECRET);
+
+        setProperty(STS_URL_KEY, getBaseUrl("securityTokenService", SBS), PUBLIC);
+
+        String serviceGatewayUrl = String.format("https://service-gw-%s.%s",
+                FasitUtils.getDefaultEnvironment(),
+                FasitUtils.getDefaultDomain(SBS)
+        );
+        setProperty(INNSYN_HENVENDELSE_WS_URL, serviceGatewayUrl, PUBLIC);
+        setProperty(HENVENDELSE_WS_URL, serviceGatewayUrl, PUBLIC);
+        setProperty(SEND_INN_HENVENDELSE_WS_URL, serviceGatewayUrl, PUBLIC);
+        setProperty(BRUKERPROFIL_V_3_URL, serviceGatewayUrl, PUBLIC);
+
+        setProperty(AZUREAD_B2C_DISCOVERY_URL_PROPERTY_NAME, getBaseUrl("aad_b2c_discovery"), PUBLIC);
+        setProperty(AZUREAD_B2C_EXPECTED_AUDIENCE_PROPERTY_NAME, getServiceUser("aad_b2c_clientid", APPLICATION_NAME).getUsername(), PUBLIC);
+
+        Main.main(args);
+    }
+}
