86f6b1d500068b3467d1e8b4ccc2726be43696d8
Nicklas Utgaard <nutgaard@gmail.com>
2020-04-02 13:35:30 +0200

    [KAIZEN-0] sender med identtype

--- src/main/java/no/nav/sbl/dialogarena/mininnboks/consumer/PersonService.java
+++ src/main/java/no/nav/sbl/dialogarena/mininnboks/consumer/PersonService.java
@@ -14,6 +14,7 @@ import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3;
 import no.nav.tjeneste.virksomhet.person.v3.informasjon.GeografiskTilknytning;
 import no.nav.tjeneste.virksomhet.person.v3.informasjon.NorskIdent;
 import no.nav.tjeneste.virksomhet.person.v3.informasjon.PersonIdent;
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Personidenter;
 import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentGeografiskTilknytningRequest;
 import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentGeografiskTilknytningResponse;
 import org.slf4j.Logger;
@@ -32,8 +33,10 @@ public interface PersonService {
     class Default implements PersonService {
         static final Logger logger = LoggerFactory.getLogger(PersonService.class);
 
-        static final WSPersonidenter identtype = new WSPersonidenter()
+        static final WSPersonidenter brukerprofilIdentType = new WSPersonidenter()
                 .withKodeRef("http://nav.no/kodeverk/Term/Personidenter/FNR/nb/F_c3_b8dselnummer?v=1")
+                .withValue("FNR");
+        static final Personidenter personV3IdentType = new Personidenter()
                 .withValue("FNR");
         private final BrukerprofilV3 brukerprofilV3;
         private final PersonV3 personV3;
@@ -47,7 +50,7 @@ public interface PersonService {
         @SneakyThrows
         public Optional<String> hentEnhet() {
             String fnr = SubjectHandler.getIdent().orElseThrow(() -> new NotAuthorizedException("Fant ikke brukers OIDC-token"));
-            WSNorskIdent ident = new WSNorskIdent().withType(identtype).withIdent(fnr);
+            WSNorskIdent ident = new WSNorskIdent().withType(brukerprofilIdentType).withIdent(fnr);
             WSHentKontaktinformasjonOgPreferanserRequest kontaktRequest = new WSHentKontaktinformasjonOgPreferanserRequest().withIdent(ident);
             WSPerson person = brukerprofilV3.hentKontaktinformasjonOgPreferanser(kontaktRequest).getBruker();
             WSBruker bruker = (WSBruker) person;
@@ -75,7 +78,7 @@ public interface PersonService {
         }
 
         private PersonIdent lagAktoer(String ident) {
-            return new PersonIdent().withIdent(new NorskIdent().withIdent(ident));
+            return new PersonIdent().withIdent(new NorskIdent().withType(personV3IdentType).withIdent(ident));
         }
     }
 }
