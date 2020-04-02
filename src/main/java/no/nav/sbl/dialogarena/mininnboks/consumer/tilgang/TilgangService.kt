45bb021f70f57bd6db11a44d96076632c9371393
Nicklas Utgaard <nutgaard@gmail.com>
2020-04-02 11:14:53 +0200

    [KAIZEN-0] logger litt informasjon om eneht

--- src/main/java/no/nav/sbl/dialogarena/mininnboks/consumer/tilgang/TilgangService.kt
+++ src/main/java/no/nav/sbl/dialogarena/mininnboks/consumer/tilgang/TilgangService.kt
@@ -3,6 +3,7 @@ package no.nav.sbl.dialogarena.mininnboks.consumer.tilgang
 import no.nav.sbl.dialogarena.mininnboks.Try
 import no.nav.sbl.dialogarena.mininnboks.consumer.PersonService
 import no.nav.sbl.dialogarena.mininnboks.consumer.pdl.PdlService
+import org.slf4j.LoggerFactory
 
 data class TilgangDTO(val resultat: Resultat, val melding: String) {
     enum class Resultat {
@@ -18,12 +19,16 @@ class TilgangServiceImpl(
         private val pdlService: PdlService,
         private val personService: PersonService
 ) : TilgangService {
+    private val log = LoggerFactory.getLogger(TilgangService::class.java)
+
     override fun harTilgangTilKommunalInnsending(fnr: String): TilgangDTO {
-        val harEnhet = Try.of { personService.hentEnhet().filter { it.isNotBlank() } .isPresent }
+        val harEnhet = Try.of { personService.hentEnhet().filter { it.isNotBlank() }.isPresent }
         if (harEnhet.isFailure()) {
             return TilgangDTO(TilgangDTO.Resultat.FEILET, "Kunne ikke hente brukers enhet: ${harEnhet.getFailure().message}")
         } else if (!harEnhet.get()) {
             return TilgangDTO(TilgangDTO.Resultat.INGEN_ENHET, "Bruker har ingen enhet")
+        } else {
+            log.info("Fant enhet for bruker: ${harEnhet.get()}")
         }
 
         val harKode6 = Try.of { pdlService.harKode6(fnr) }
