40c28115bda80c3f44b8557002502880f069215d
ankur <H3rl3k1r123>
2020-09-18 12:38:20 +0200

    Env variable endringer

--- src/main/kotlin/no/nav/sbl/dialogarena/mininnboks/HttpHandler.kt
+++ src/main/kotlin/no/nav/sbl/dialogarena/mininnboks/HttpHandler.kt
@@ -1,31 +1,38 @@
 package no.nav.sbl.dialogarena.mininnboks
 
-import io.ktor.application.install
-import io.ktor.auth.Authentication
-import io.ktor.auth.jwt.jwt
-import io.ktor.features.CORS
-import io.ktor.features.CallLogging
-import io.ktor.features.ContentNegotiation
-import io.ktor.features.StatusPages
-import io.ktor.http.ContentType
-import io.ktor.http.HttpMethod
-import io.ktor.jackson.JacksonConverter
-import io.ktor.metrics.dropwizard.DropwizardMetrics
-import io.ktor.request.path
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.jackson.*
+import io.ktor.metrics.dropwizard.*
+import io.ktor.request.*
 import io.ktor.routing.*
-import io.ktor.server.netty.Netty
-import io.ktor.server.engine.ApplicationEngine
-import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 import io.prometheus.client.dropwizard.DropwizardExports
+import no.nav.common.utils.EnvironmentUtils
+import no.nav.common.utils.EnvironmentUtils.setProperty
+import no.nav.common.utils.NaisUtils
 import no.nav.sbl.dialogarena.mininnboks.ObjectMapperProvider.Companion.objectMapper
 import no.nav.sbl.dialogarena.mininnboks.provider.rest.henvendelse.henvendelseController
 import no.nav.sbl.dialogarena.mininnboks.provider.rest.naisRoutes
 import no.nav.sbl.dialogarena.mininnboks.provider.rest.resources.resourcesController
 import no.nav.sbl.dialogarena.mininnboks.provider.rest.tilgang.tilgangController
 import no.nav.sbl.dialogarena.mininnboks.provider.rest.ubehandletmelding.sporsmalController
-
 import org.slf4j.event.Level
 import no.nav.sbl.dialogarena.mininnboks.JwtUtil.Companion as JwtUtil
+
+const val FSS_SRVMININNBOKS_USERNAME = "FSS_SRVMININNBOKS_USERNAME"
+const val FSS_SRVMININNBOKS_PASSWORD = "FSS_SRVMININNBOKS_PASSWORD"
+const val SRVMININNBOKS_USERNAME = "SRVMININNBOKS_USERNAME"
+const val SRVMININNBOKS_PASSWORD = "SRVMININNBOKS_PASSWORD"
+const val PDL_API_URL = "PDL_API_URL"
+const val PDL_API_APIKEY = "PDL_API_APIKEY"
+const val STS_APIKEY = "STS_APIKEY"
+private const val DEFAULT_SECRETS_BASE_PATH = "/var/run/secrets/nais.io"
+
 
 fun createHttpServer(applicationState: ApplicationState,
                      configuration: Configuration,
@@ -66,6 +73,9 @@ fun createHttpServer(applicationState: ApplicationState,
         filter { call -> call.request.path().contains("/") }
     }
 
+
+    loadVaultSecrets()
+    loadApigwKeys()
     val serviceConfig = ServiceConfig(configuration)
     val henvendelseService = serviceConfig.henvendelseService(serviceConfig.personService())
 
@@ -84,4 +94,23 @@ fun createHttpServer(applicationState: ApplicationState,
     }
 
     applicationState.initialized = true
+}
+
+private fun loadVaultSecrets() {
+    val fssServiceUser = NaisUtils.getCredentials("srvmininnboks-fss")
+    setProperty(FSS_SRVMININNBOKS_USERNAME, fssServiceUser.username, EnvironmentUtils.Type.PUBLIC)
+    setProperty(FSS_SRVMININNBOKS_PASSWORD, fssServiceUser.password, EnvironmentUtils.Type.SECRET)
+    val serviceUser = NaisUtils.getCredentials("srvmininnboks")
+    setProperty(SRVMININNBOKS_USERNAME, serviceUser.username, EnvironmentUtils.Type.PUBLIC)
+    setProperty(SRVMININNBOKS_PASSWORD, serviceUser.password, EnvironmentUtils.Type.SECRET)
+}
+
+private fun loadApigwKeys() {
+    setProperty(PDL_API_APIKEY, getApigwKey("pdl-api"), EnvironmentUtils.Type.SECRET)
+    setProperty(STS_APIKEY, getApigwKey("security-token-service-token"), EnvironmentUtils.Type.SECRET)
+}
+
+private fun getApigwKey(producerApp: String): String? {
+    val location = java.lang.String.format("%s/apigw/%s/x-nav-apiKey", DEFAULT_SECRETS_BASE_PATH, producerApp)
+    return NaisUtils.getFileContent(location)
 }
