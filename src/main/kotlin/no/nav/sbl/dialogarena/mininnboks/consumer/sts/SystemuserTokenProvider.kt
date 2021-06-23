b1ed6d6d9516a8ec11654278ca47d971be4839ca
vildestefferud <qwartz.ms@hotmail.com>
2021-06-23 10:07:33 +0200

    [OPP-1271] endrer til å bruke ktor

--- src/main/kotlin/no/nav/sbl/dialogarena/mininnboks/consumer/sts/SystemuserTokenProvider.kt
+++ src/main/kotlin/no/nav/sbl/dialogarena/mininnboks/consumer/sts/SystemuserTokenProvider.kt
@@ -2,19 +2,13 @@ package no.nav.sbl.dialogarena.mininnboks.consumer.sts
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.nimbusds.jwt.JWT
 import com.nimbusds.jwt.JWTParser
 import io.ktor.client.request.*
 import kotlinx.coroutines.runBlocking
 import no.nav.common.auth.oidc.discovery.OidcDiscoveryConfiguration
 import no.nav.common.log.MDCConstants
-import no.nav.common.rest.client.RestClient
-import no.nav.common.rest.client.RestUtils
-import no.nav.sbl.dialogarena.mininnboks.JacksonUtils
 import no.nav.sbl.dialogarena.mininnboks.ktorClient
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import java.text.ParseException
@@ -31,16 +25,14 @@ interface SystemuserTokenProvider {
             srvUsername: String,
             srvPassword: String,
             stsApiKey: String,
-            client: OkHttpClient = RestClient.baseClientBuilder().build()
-        ): SystemuserTokenProvider = SystemuserTokenProviderImpl(true, discoveryUrl, srvUsername, srvPassword, stsApiKey, client)
+        ): SystemuserTokenProvider = SystemuserTokenProviderImpl(true, discoveryUrl, srvUsername, srvPassword, stsApiKey)
 
         fun fromTokenEndpoint(
             tokenEndpointUrl: String,
             srvUsername: String,
             srvPassword: String,
             stsApiKey: String,
-            client: OkHttpClient = RestClient.baseClientBuilder().build()
-        ): SystemuserTokenProvider = SystemuserTokenProviderImpl(false, tokenEndpointUrl, srvUsername, srvPassword, stsApiKey, client)
+        ): SystemuserTokenProvider = SystemuserTokenProviderImpl(false, tokenEndpointUrl, srvUsername, srvPassword, stsApiKey)
     }
 
     fun getSystemUserAccessToken(): String?
@@ -52,8 +44,7 @@ class SystemuserTokenProviderImpl internal constructor(
     startingUrl: String,
     private val srvUsername: String,
     private val srvPassword: String,
-    private val stsApiKey: String,
-    private val client: OkHttpClient
+    private val stsApiKey: String
 ) : SystemuserTokenProvider {
 
     private val log = LoggerFactory.getLogger(SystemuserTokenProvider::class.java)
@@ -85,27 +76,13 @@ class SystemuserTokenProviderImpl internal constructor(
         val targetUrl = "$tokenEndpointUrl?grant_type=client_credentials&scope=openid"
         val basicAuth: String = basicCredentials(srvUsername, srvPassword)
 
-        val request: Request = Request.Builder()
-            .url(targetUrl)
-            .addHeader("Nav-Call-Id", MDC.get(MDCConstants.MDC_CALL_ID))
-            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED)
-            .header(HttpHeaders.AUTHORIZATION, basicAuth)
-            .header("x-nav-apiKey", stsApiKey)
-            .get()
-            .build()
-
-        val response = client.newCall(request).execute()
-
-        if (response.code() != 200) {
-            val strResponse = RestUtils.parseJsonResponse(response, String::class.java)
-            val errorMessage = String.format("Received unexpected status %d when requesting access token for system user. Response: %s", response.code(), strResponse)
-            log.error("Failed to get SystemUserToken: $errorMessage")
-            throw RuntimeException(errorMessage)
-        } else {
-            val body = response.body()?.string()
-            val credentials = body?.let { JacksonUtils.objectMapper.readValue<ClientCredentialsResponse>(it) }
-            log.info("Fetched SystemUserToken, parts: ${credentials?.accessToken?.split(".")?.size}")
-            return credentials
+        return runBlocking {
+            ktorClient.get<ClientCredentialsResponse>(targetUrl) {
+                header("Nav-Call-Id", MDC.get(MDCConstants.MDC_CALL_ID))
+                header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED)
+                header(HttpHeaders.AUTHORIZATION, basicAuth)
+                header("x-nav-apiKey", stsApiKey)
+            }
         }
     }
 }
